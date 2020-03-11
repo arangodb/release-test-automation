eba8e38c0512011c513d89300bd25966e6a44aab
willi <willi@arangodb.com>
2020-03-11 17:11:04 +0000

    non/enterprise mode

--- attic_snippets/checkStripped.py
+++ attic_snippets/checkStripped.py
@@ -38,19 +38,43 @@ def runFileCommand(fileToCheck):
     Popen=subprocess.Popen
     p = Popen(['file', fileToCheck], stdout=PIPE, stdin=PIPE, stderr=PIPE, universal_newlines=True)
     l = p.stdout.readline()
-    print(l)
+    # print(l)
     return l
 
+isEnterprise = sys.argv[1] == 'enterprise'
+
+def isEnterpriseFile(fileToCheck):
+    try:
+        enterpriseBinaries.index(fileToCheck)
+        return True
+    except ValueError:
+        return False
+
 for symlink in installSymlinks:
+    if isEnterpriseFile(symlink) and not isEnterprise:
+        if os.path.exists(symlink):
+            raise Exception('enterprise file found in community install!: ' + symlink)
+        else:
+            continue
     if not os.path.islink(symlink):
         raise Exception(" expected " + symlink + " to be a symlink ")
 
 for strippedFile in installStrippedBinaries:
+    if isEnterpriseFile(strippedFile) and not isEnterprise:
+        if os.path.exists(strippedFile):
+            raise Exception('enterprise file found in community install!: ' + strippedFile)
+        else:
+            continue
     output = runFileCommand(strippedFile)
     if output.find(', stripped') < 0:
         raise Exception("expected " + strippedFile + " to be stripped, but its not: " + output)
 
 for nonStrippedFile in installNonStrippedBinaries:
+    if isEnterpriseFile(nonStrippedFile) and not isEnterprise:
+        if os.path.exists(nonStrippedFile):
+            raise Exception('enterprise file found in community install!: ' + nonStrippedFile)
+        else:
+            continue
     output = runFileCommand(nonStrippedFile)
     if output.find(', not stripped') < 0:
         raise Exception("expected " + nonStrippedFile + " to be stripped, but its not: " + output)
