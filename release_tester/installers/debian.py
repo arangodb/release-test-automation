49d39f478f4f92fc881f7d35a831fe88d68608de
Wilfried Goesgens <willi@arangodb.com>
2020-03-27 09:18:51 +0100

    remove not needed files

--- release_tester/installers/debian.py
+++
@@ -1,4 +0,0 @@
-from baseinstaller import installer;
-
-class debianInstaller(installer):
-    def __init(self):
--- release_tester/releasetest.py
+++
@@ -1,9 +0,0 @@
-
-
-from baseinstaller import installer
-
-class debianInstaller(installer):
-    def __init__(self):
-
-    def runInstaller(self):
-        print("install!")
