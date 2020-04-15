463c38585ff31995dd6568cc67f0eb9c856b2de1
Wilfried Goesgens <willi@arangodb.com>
2020-04-15 12:44:27 +0200

    add timestamp tool

---
+++ release_tester/tools/timestamp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" return formated timestamp"""
+import datetime
+
+
+def timestamp():
+    """ get the formated "now" timestamp"""
+    return datetime.datetime.utcnow().isoformat()
