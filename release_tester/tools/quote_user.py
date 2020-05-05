9950e57044168e06be7124530ba8da62f101213d
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-05-05 10:09:09 +0200

    wrap line

--- release_tester/tools/quote_user.py
+++ release_tester/tools/quote_user.py
@@ -7,8 +7,12 @@ def quote_user(cfg):
     """ print all available frontends, and wait for the user to confirm (if) """
     for frontend in cfg.frontends:
         print(frontend)
-        logging.info('frontend can be reached at: {f.proto}://root:{c.passvoid}@{f.ip}:{f.port}{path}'.format(
-                        f=frontend, c=cfg, path='/_db/_system/_admin/aardvark/index.html#login')
+        logging.info('frontend can be reached at:' +
+                     '{f.proto}://root:{c.passvoid}@{f.ip}:{f.port}{path}'.format(
+                        f=frontend,
+                        c=cfg,
+                        path='/_db/_system/_admin/aardvark/index.html#login'
+                      )
                     )
     if cfg.quote_user:
         input("Press Enter to continue")
