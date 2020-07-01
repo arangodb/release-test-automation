613a729f9c37be9411522bcdd28832c3c0e8fc72
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2020-07-01 14:58:49 +0200

    Finalized Tar package Linux or Mac installer

--- release_tester/arangodb/installers/tar.py
+++ release_tester/arangodb/installers/tar.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
-""" run an Tar installer for the debian based operating system """
-import sys
-import time
-import os
+""" run an Tar installer for the Linux/Mac based operating system """
+import platform
 import shutil
 import logging
 from pathlib import Path
 import psutil
-from arangodb.instance import ArangodInstance
-from arangodb.installers.base import InstallerBase
-from tools.asciiprint import ascii_print
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-import tools.loghelper as lh
 import semver
+import tools.loghelper as lh
+from arangodb.installers.base import InstallerBase
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 
 class InstallerTAR(InstallerBase):
-    """ install tar.gz on debian or ubuntu hosts """
+    """ install Tar.gz's on Linux/Mac hosts """
     def __init__(self, cfg):
+        macver = platform.mac_ver()
+        if macver[0]:
+            self.tar = 'gtar'
+            cfg.localhost = 'localhost'
+        else:
+            self.tar = 'tar'
+            cfg.localhost = 'ip6-localhost'
 
-        self.tar = 'tar'
         self.check_stripped = True
         self.check_symlink = True
         self.server_package = None
@@ -39,27 +41,27 @@ class InstallerTAR(InstallerBase):
         cfg.real_sbin_dir = None
         cfg.localhost = 'ip6-localhost'
 
-        cfg.logDir = None
+        cfg.logDir = Path()
         cfg.dbdir = None
         cfg.appdir = None
         cfg.cfgdir = None
 
-
         super().__init__(cfg)
 
-    def calculate_package_names(self):  
-        # Are those required to be stored in the cfg?
+    def calculate_package_names(self):
         enterprise = 'e' if self.cfg.enterprise else ''
-        # package_version = '1'
         architecture = 'linux'
+        macver = platform.mac_ver()
+        if macver[0]:
+            architecture = 'macos'
 
         self.desc = {
             "ep"   : enterprise,
             "ver"  : self.cfg.version,
             "arch" : architecture
         }
-        self.server_package = 'arangodb3{ep}-{arch}-{ver}.tar.gz'.format(**self.desc)
 
+        self.server_package = 'arangodb3{ep}-{arch}-{ver}.tar.gz'.format(**self.desc)
         self.cfg.installPrefix = Path("/tmp")/'arangodb3{ep}-{ver}'.format(**self.desc)
         self.cfg.bin_dir = self.cfg.installPrefix / "usr" / "bin"     # /usr/bin
         self.cfg.sbin_dir = self.cfg.installPrefix / "usr" / "sbin"   # /usr/sbin
@@ -68,44 +70,16 @@ class InstallerTAR(InstallerBase):
 
     def check_service_up(self):
         pass
-        # time.sleep(1)    # TODO
-        # print("I am here in: deb.py line: 69")
-        # return True
 
     def start_service(self):
         pass
-        # print("I am here in: deb.py line: 73")
-
-        # startserver = pexpect.spawnu('service arangodb3 start')
-        # logging.debug("waiting for eof")
-        # startserver.expect(pexpect.EOF, timeout=30)
-        # while startserver.isalive():
-        #     print('.', end='')
-        #     if startserver.exitstatus != 0:
-        #         raise Exception("server service start didn't"
-        #                         "finish successfully!")
-        # time.sleep(0.1)
-        # self.instance.detect_pid(1) # should be owned by init
 
     def stop_service(self):
         pass
 
-        # print("I am here in: tar.py line: 88")
-
-        # stopserver = pexpect.spawnu('service arangodb3 stop')
-        # logging.debug("waiting for eof")
-        # stopserver.expect(pexpect.EOF, timeout=30)
-        # while stopserver.isalive():
-        #     print('.', end='')
-        #     if stopserver.exitstatus != 0:
-        #         raise Exception("server service stop didn't"
-        #                         "finish successfully!")
-
     def upgrade_package(self):
-       
-        # Tar upgrade installer is the same way we did for installing.
+        """ Tar installer is the same way we did for installing."""
         self.install_package()
-        
 
     def install_package(self):
         logging.info("installing Arangodb debian Tar package")
@@ -118,23 +92,8 @@ class InstallerTAR(InstallerBase):
         print()
         logging.info('Installation successfull')
 
-        
     def un_install_package(self):
-        pass
-        # print("I am here in: tar.py line: 199")
-
-        # uninstall = pexpect.spawnu('dpkg --purge ' +
-        #                            'arangodb3' +
-        #                            ('e' if self.cfg.enterprise else ''))
-
-        # try:
-        #     uninstall.expect('Purging')
-        #     ascii_print(uninstall.before)
-        #     uninstall.expect(pexpect.EOF)
-        #     ascii_print(uninstall.before)
-        # except pexpect.exceptions.EOF:
-        #     ascii_print(uninstall.before)
-        #     sys.exit(1)
+        shutil.rmtree(self.cfg.installPrefix)
 
     def broadcast_bind(self):
         pass
@@ -147,13 +106,3 @@ class InstallerTAR(InstallerBase):
 
     def cleanup_system(self):
         pass
-        # print("I am here in: tar.py line: 215")
-        # # TODO: should this be cleaned by the deb uninstall in first place?
-        # if self.cfg.logDir.exists():
-        #     shutil.rmtree(self.cfg.logDir)
-        # if self.cfg.dbdir.exists():
-        #     shutil.rmtree(self.cfg.dbdir)
-        # if self.cfg.appdir.exists():
-        #     shutil.rmtree(self.cfg.appdir)
-        # if self.cfg.cfgdir.exists():
-        #     shutil.rmtree(self.cfg.cfgdir)
