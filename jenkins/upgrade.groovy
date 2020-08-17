a5ea5749941d9615fb7aea18c78d142719340876
Wilfried Goesgens <willi@arangodb.com>
2020-08-17 14:25:25 +0200

    make absolute

--- jenkins/upgrade.groovy
+++ jenkins/upgrade.groovy
@@ -66,14 +66,14 @@ node(TARGET_HOST)  {
     
     if (params['VERSION_OLD'] != "") {
         ACQUIRE_COMMAND = """
-${PYTHON} release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-magic ${params['ENTERPRISE_KEY']} --package-dir ${PACKAGE_DIR} ${FORCE_PARAM_OLD} --source ${params['PACKAGE_SOURCE_OLD']} --version '${params['VERSION_OLD']}' --httpuser dothebart --httppassvoid '${params['HTTP_PASSVOID']}' ${ZIP}
+${PYTHON} ${WORKSPACE}/release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-magic ${params['ENTERPRISE_KEY']} --package-dir ${PACKAGE_DIR} ${FORCE_PARAM_OLD} --source ${params['PACKAGE_SOURCE_OLD']} --version '${params['VERSION_OLD']}' --httpuser dothebart --httppassvoid '${params['HTTP_PASSVOID']}' ${ZIP}
 """
         print("downloading old package(s) using:")
         print(ACQUIRE_COMMAND)
         sh ACQUIRE_COMMAND
     }
     ACQUIRE_COMMAND = """
-${PYTHON} release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-magic ${params['ENTERPRISE_KEY']} --package-dir ${PACKAGE_DIR} ${FORCE_PARAM_NEW} --source ${params['PACKAGE_SOURCE_NEW']} --version '${params['VERSION_NEW']}' --httpuser dothebart --httppassvoid '${params['HTTP_PASSVOID']}' ${ZIP}
+${PYTHON} ${WORKSPACE}/release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-magic ${params['ENTERPRISE_KEY']} --package-dir ${PACKAGE_DIR} ${FORCE_PARAM_NEW} --source ${params['PACKAGE_SOURCE_NEW']} --version '${params['VERSION_NEW']}' --httpuser dothebart --httppassvoid '${params['HTTP_PASSVOID']}' ${ZIP}
 """
     print("downloading new package(s) using:")
     print(ACQUIRE_COMMAND)
@@ -82,7 +82,7 @@ ${PYTHON} release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-ma
     if (fileExists('/tmp/config.yml')) {
         print("cleaning up the system (if):")
         CLEANUP_COMMAND = """
-${SUDO} ${PYTHON} release_tester/cleanup.py ${ZIP}
+${SUDO} ${PYTHON} ${WORKSPACE}/release_tester/cleanup.py ${ZIP}
 """
         print(CLEANUP_COMMAND)
         sh CLEANUP_COMMAND
@@ -93,7 +93,7 @@ ${SUDO} ${PYTHON} release_tester/cleanup.py ${ZIP}
     if (params['VERSION_OLD'] != "") {
         print("Running upgrade test")
         UPGRADE_COMMAND = """
-${SUDO} ${PYTHON} release_tester/upgrade.py ${ENTERPRISE_PARAM} --old-version ${params['VERSION_OLD']} --version ${params['VERSION_NEW']} --package-dir ${PACKAGE_DIR} --publicip 192.168.173.88 ${ZIP} --no-interactive
+${SUDO} ${PYTHON} ${WORKSPACE}/release_tester/upgrade.py ${ENTERPRISE_PARAM} --old-version ${params['VERSION_OLD']} --version ${params['VERSION_NEW']} --package-dir ${PACKAGE_DIR} --publicip 192.168.173.88 ${ZIP} --no-interactive
 """
         print(UPGRADE_COMMAND)
         sh UPGRADE_COMMAND
@@ -101,7 +101,7 @@ ${SUDO} ${PYTHON} release_tester/upgrade.py ${ENTERPRISE_PARAM} --old-version ${
     } else {
         print("Running plain install test")
         TEST_COMMAND = """
-${SUDO} ${PYTHON} release_tester/test.py ${ENTERPRISE_PARAM} --version ${params['VERSION_NEW']} --package-dir ${PACKAGE_DIR} --publicip 192.168.173.88 ${ZIP} --no-interactive
+${SUDO} ${PYTHON} ${WORKSPACE}/release_tester/test.py ${ENTERPRISE_PARAM} --version ${params['VERSION_NEW']} --package-dir ${PACKAGE_DIR} --publicip 192.168.173.88 ${ZIP} --no-interactive
 """
         print(TEST_COMMAND)
         sh TEST_COMMAND
