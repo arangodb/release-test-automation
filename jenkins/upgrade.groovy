22558b44bc1902c2d73b55d674ebb836894cc8f5
Wilfried Goesgens <willi@arangodb.com>
2020-08-20 14:06:54 +0200

    add wintendo

--- jenkins/upgrade.groovy
+++ jenkins/upgrade.groovy
@@ -1,4 +1,5 @@
 PYTHON='python3'
+PACKAGE_DIR = '/home/jenkins/Downloads/'
 
 switch (TARGET) {
     case 'windows': 
@@ -6,6 +7,7 @@ switch (TARGET) {
         TARGET_HOST='bruecktendo'
         SUDO=''
         TARGET_HOST = 'packagestest-windows'
+        PACKAGE_DIR = 'c:/jenkins/downloads'
         break
 
     case 'linux_deb':
@@ -20,7 +22,6 @@ switch (TARGET) {
         break
 }
 
-PACKAGE_DIR = '/home/jenkins/Downloads/'
 
 ENTERPRISE_PARAM = '--no-enterprise'
 if (params['ENTERPRISE']) {
@@ -71,7 +72,7 @@ node(TARGET_HOST)  {
     stage('fetch old') {
         if (params['VERSION_OLD'] != "") {
             ACQUIRE_COMMAND = """
-${PYTHON} ${WORKSPACE}/release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-magic ${params['ENTERPRISE_KEY']} --package-dir ${PACKAGE_DIR} ${FORCE_PARAM_OLD} --source ${params['PACKAGE_SOURCE_OLD']} --version '${params['VERSION_OLD']}' --httpuser dothebart --httppassvoid '${params['HTTP_PASSVOID']}' ${ZIP} ${VERBOSE}
+${PYTHON} ${WORKSPACE}/release_tester/acquire_packages.py ${ENTERPRISE_PARAM} --enterprise-magic ${params['ENTERPRISE_KEY']} --package-dir ${PACKAGE_DIR} ${FORCE_PARAM_OLD} --source ${params['PACKAGE_SOURCE_OLD']} --version "${params['VERSION_OLD']}" --httpuser dothebart --httppassvoid "${params['HTTP_PASSVOID']}" ${ZIP} ${VERBOSE}
 """
             print("downloading old package(s) using:")
             print(ACQUIRE_COMMAND)
