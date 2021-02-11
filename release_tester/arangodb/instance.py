2724eed24cc9bb7a417f2f4c7b0614e1eeaa1c46
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2021-02-11 17:15:46 +0600

    Fatal error check for mac upgrade installer

--- release_tester/arangodb/instance.py
+++ release_tester/arangodb/instance.py
@@ -262,6 +262,10 @@ arangod instance
                     # skip empty lines
                     if line == "":
                         continue
+                    if "] FATAL [" in line:
+                        print('Error: ', line)
+                        raise Exception("FATAL error found in arangod.log.")
+                        break
                     # save last line and append to string (why not slurp the whole file?)
                     last_line = line
                     log_file_content += '\n' + line
