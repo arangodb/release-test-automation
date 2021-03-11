815e7034e8c2d34c6fc279e1e67992be35effb7a
Wilfried Goesgens <willi@arangodb.com>
2021-03-11 16:33:15 +0100

    dummy

--- release_tester/arangodb/starter/deployments/selenium_deployments/dc2dc.py
+++ release_tester/arangodb/starter/deployments/selenium_deployments/dc2dc.py
@@ -6,3 +6,19 @@ class Dc2Dc(SeleniumRunner):
     """ check the leader follower setup and its properties """
     def __init__(self, webdriver):
         super().__init__(webdriver)
+
+    def check_old(self, cfg):
+        """ check the integrity of the old system before the upgrade """
+        pass
+
+    def upgrade_deployment(self, old_cfg, new_cfg):
+        """ check the upgrade whether the versions in the table switch etc. """
+        pass
+
+    def jam_step_1(self, cfg):
+        """ check the integrity of the old system before the upgrade """
+        pass
+
+    def jam_step_2(self, cfg):
+        """ check the integrity of the old system before the upgrade """
+        pass
