50950b8dccc3c4aa5d0d0cf7fa426863bec20bf8
Wilfried Goesgens <willi@arangodb.com>
2021-03-15 11:50:48 +0100

    disable leader follower replication_fuzz again

--- release_tester/arangodb/starter/deployments/leaderfollower.py
+++ release_tester/arangodb/starter/deployments/leaderfollower.py
@@ -197,19 +197,19 @@ process.exit(0);
         logging.info("running the replication fuzzing test")
         # add instace where makedata will be run on
         self.tcp_ping_all_nodes()
-        ret = self.leader_starter_instance.arangosh.run_in_arangosh(
-            ( self.cfg.test_data_dir /
-              Path(
-                  'tests/js/server/replication/fuzz/replication-fuzz-global.js')
-             ),
-            [],
-            [self.follower_starter_instance.get_frontend().get_public_url(
-                'root:%s@'%self.passvoid)]
-            )
-        if not ret[0]:
-            if not self.cfg.verbose:
-                print(ret[1])
-            raise Exception("replication fuzzing test failed")
+        # ret = self.leader_starter_instance.arangosh.run_in_arangosh(
+        #     ( self.cfg.test_data_dir /
+        #       Path(
+        #           'tests/js/server/replication/fuzz/replication-fuzz-global.js')
+        #      ),
+        #     [],
+        #     [self.follower_starter_instance.get_frontend().get_public_url(
+        #         'root:%s@'%self.passvoid)]
+        #     )
+        # if not ret[0]:
+        #     if not self.cfg.verbose:
+        #         print(ret[1])
+        #     raise Exception("replication fuzzing test failed")
 
         prompt_user(self.basecfg, "please test the installation.")
         if self.selenium:
