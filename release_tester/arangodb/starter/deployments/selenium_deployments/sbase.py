ff7118fb37c403c0a70742a2ff48bf77d80ddb70
Wilfried Goesgens <willi@arangodb.com>
2021-03-19 10:31:43 +0100

    make sure that the database selection dropdown is actually filled

--- release_tester/arangodb/starter/deployments/selenium_deployments/sbase.py
+++ release_tester/arangodb/starter/deployments/selenium_deployments/sbase.py
@@ -74,10 +74,19 @@ class SeleniumRunner(ABC):
             elem.send_keys(frontend_instance[0].get_passvoid())
             elem.send_keys(Keys.RETURN)
             print("S: logging in")
+            while True:
+                elem = WebDriverWait(self.web, 15).until(
+                    EC.presence_of_element_located((By.ID, "loginDatabase"))
+                )
+                txt = elem.text
+                if txt.find('_system') < 0:
+                    print('S: _system not found in ' + txt + ' ; retrying!')
+                    time.sleep(2)
+                else:
+                    break
             elem = WebDriverWait(self.web, 15).until(
                 EC.presence_of_element_located((By.ID, "goToDatabase"))
             )
-            time.sleep(2)
             elem.click()
             print("S: we're in!")
 
