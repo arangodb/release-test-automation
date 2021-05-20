e707aae90ae205e6e724098885935e12d79de2d5
Wilfried Goesgens <willi@arangodb.com>
2021-05-20 15:13:01 +0200

    lint

--- release_tester/arangodb/starter/deployments/selenium_deployments/cluster_perf.py
+++ release_tester/arangodb/starter/deployments/selenium_deployments/cluster_perf.py
@@ -2,12 +2,11 @@
 """ test the UI of a leader follower setup """
 from arangodb.starter.deployments.selenium_deployments.sbase import SeleniumRunner
 
-class LeaderFollower(SeleniumRunner,
-                     testrun_name: str):
+class ClusterPerf(SeleniumRunner):
     """ check the leader follower setup and its properties """
     def __init__(self, webdriver,
-                 is_headless,
-                 testrun_name):
+                 is_headless: bool,
+                 testrun_name: str):
         super().__init__(webdriver,
                          is_headless,
                          testrun_name)
