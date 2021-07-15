77f151a77bbeae51305f3be74db3e3b1550d00fe
Wilfried Goesgens <willi@arangodb.com>
2021-07-15 17:01:15 +0200

    revert test probe

--- release_tester/arangodb/starter/deployments/cluster.py
+++ release_tester/arangodb/starter/deployments/cluster.py
@@ -105,7 +105,6 @@ db.testCollection.save({test: "document"})
         self.set_frontend_instances()
 
     def test_setup_impl(self):
-        raise Exception('test')
         pass
 
     def wait_for_restore_impl(self, backup_starter):
