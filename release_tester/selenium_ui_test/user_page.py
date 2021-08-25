1cea005d4bd736d2efdb83b4620f2c58ec5805ad
Wilfried Goesgens <willi@arangodb.com>
2021-08-25 13:38:18 +0200

    self

--- release_tester/selenium_ui_test/user_page.py
+++ release_tester/selenium_ui_test/user_page.py
@@ -23,6 +23,7 @@ class UserPage(BaseSelenium):
         self.enter_new_password_id = "newPassword"
         self.create_user_btn_id = "modalButton1"
         self.selecting_user_tester_id = "tester"
+        self.tester_id = '//*[@id="userManagementThumbnailsIn"]/div[3]/div/h5'
         self.permission_link_id = "//*[@id='subNavigationBar']/ul[2]/li[2]/a"
         self.db_permission_read_only = "//*[@id='*-db']/div[3]/input"
         self.db_permission_read_write = '//*[@id="*-db"]/div[2]/input'
@@ -36,107 +37,93 @@ class UserPage(BaseSelenium):
     def user_tab(self):
         """selecting user tab"""
         user_tab = self.select_user_tab_id
-        user_tab = BaseSelenium.locator_finder_by_id(self, user_tab)
+        user_tab = BaseSelenium.locator_finder_by_id(user_tab)
         user_tab.click()
 
     def add_new_user(self, tester):
         """User page selecting add new user"""
         print("New user creation begins \n")
-        self.add_new_user_id = BaseSelenium.locator_finder_by_id(self, self.add_new_user_id)
-        self.add_new_user_id.click()
+        add_new_user_sitem = self.locator_finder_by_id(self.add_new_user_id)
+        add_new_user_sitem.click()
 
         # entering new user name
-        self.enter_new_user_name_id = BaseSelenium.locator_finder_by_id(self, self.enter_new_user_name_id)
-        self.enter_new_user_name_id.click()
-        self.enter_new_user_name_id.send_keys(tester)
+        enter_new_user_name_sitem = self.locator_finder_by_id(self.enter_new_user_name_id)
+        enter_new_user_name_sitem.click()
+        enter_new_user_name_sitem.send_keys(tester)
         time.sleep(3)
 
         # providing new user name
-        self.enter_new_name_id = BaseSelenium.locator_finder_by_id(self, self.enter_new_name_id)
-        self.enter_new_name_id.click()
-        self.enter_new_name_id.send_keys(tester)
+        enter_new_name_sitem = self.locator_finder_by_id(self.enter_new_name_id)
+        enter_new_name_sitem.click()
+        enter_new_name_sitem.send_keys(tester)
 
         # entering new user password
-        self.enter_new_password_id = BaseSelenium.locator_finder_by_id(self, self.enter_new_password_id)
-        self.enter_new_password_id.click()
-        self.enter_new_password_id.send_keys(tester)
+        enter_new_password_sitem = self.locator_finder_by_id(self.enter_new_password_id)
+        enter_new_password_sitem.click()
+        enter_new_password_sitem.send_keys(tester)
 
         # User page selecting add new user
-        self.create_user_btn_id = BaseSelenium.locator_finder_by_id(self, self.create_user_btn_id)
-        self.create_user_btn_id.click()
+        create_user_btn_sitem = self.locator_finder_by_id(self.create_user_btn_id)
+        create_user_btn_sitem.click()
         time.sleep(3)
         print("New user creation completed \n")
 
     def selecting_user_tester(self):
         """selecting newly created user"""
-        new_user = self.selecting_user_tester_id
-        new_user = BaseSelenium.locator_finder_by_id(self, new_user)
-        new_user.click()
+        new_user_sitem = self.locator_finder_by_id(self.selecting_user_tester_id)
+        new_user_sitem.click()
 
     def selecting_permission(self):
         """selecting newly created user"""
-        permission = self.permission_link_id
-        permission = BaseSelenium.locator_finder_by_xpath(self, permission)
-        permission.click()
+        permission_sitem = self.locator_finder_by_xpath(self.permission_link_id)
+        permission_sitem.click()
 
     def changing_db_permission_read_only(self):
         """changing permission for the new user"""
-        db_permission = self.db_permission_read_only
-        db_permission = BaseSelenium.locator_finder_by_xpath(self, db_permission)
-        db_permission.click()
+        db_permission_sitem = self.locator_finder_by_xpath(self.db_permission_read_only)
+        db_permission_sitem.click()
 
     def changing_db_permission_read_write(self):
         """changing permission for the new user"""
-        db_permission = self.db_permission_read_write
-        db_permission = BaseSelenium.locator_finder_by_xpath(self, db_permission)
-        db_permission.click()
+        db_permission_sitem = self.locator_finder_by_xpath(self.db_permission_read_write)
+        db_permission_sitem.click()
 
     # saving new settings for new user
     def saving_user_cfg(self):
-        save_button = self.saving_user_cfg_id
-        save_button = BaseSelenium.locator_finder_by_id(self, save_button)
-        save_button.click()
+        save_button_sitem = self.locator_finder_by_id(self.saving_user_cfg_id)
+        save_button_sitem.click()
         time.sleep(3)
 
     def selecting_new_user(self):
-        tester = '//*[@id="userManagementThumbnailsIn"]/div[3]/div/h5'
-        tester = BaseSelenium.locator_finder_by_xpath(self, tester)
-        tester.click()
+        tester_sitem = self.locator_finder_by_xpath(self.tester_id)
+        tester_sitem.click()
         time.sleep(4)
 
     def create_sample_collection(self, test_name):
         # selecting collection tab
         try:
-            collection_page = self.select_collection_page_id
-            collection_page = \
-                BaseSelenium.locator_finder_by_id(self, collection_page)
-            collection_page.click()
+            collection_page_sitem = self.locator_finder_by_id(self.select_collection_page_id)
+            collection_page_sitem.click()
 
             # Clicking on create new collection box
-            create_collection = self.select_create_collection_id
-            create_collection = \
-                BaseSelenium.locator_finder_by_id(self, create_collection)
-            create_collection.click()
+            create_collection_sitem = self.locator_finder_by_id(self.select_create_collection_id)
+            create_collection_sitem.click()
             time.sleep(2)
 
             # Providing new collection name
-            collection_name = self.select_new_collection_name_id
-            collection_name = \
-                BaseSelenium.locator_finder_by_id(self, collection_name)
-            collection_name.click()
-            collection_name.send_keys("testDoc")
+            collection_name_sitem = self.locator_finder_by_id(self.select_new_collection_name_id)
+            collection_name_sitem.click()
+            collection_name_sitem.send_keys("testDoc")
 
             # creating collection by tapping on save button
-            save = 'modalButton1'
-            save = BaseSelenium.locator_finder_by_id(self, save)
-            save.click()
+            save_sitem = self.locator_finder_by_id('modalButton1')
+            save_sitem.click()
 
             try:
-                notification = 'noty_body'
-                notification = self.locator_finder_by_css_selectors(notification)
+                notification_sitem = self.locator_finder_by_css_selectors('noty_body')
                 time.sleep(1)
                 expected_text = 'Collection: Collection "testDoc" successfully created.'
-                assert notification.text == expected_text, f"Expected text{expected_text} but got {notification.text}"
+                assert notification_sitem.text == expected_text, f"Expected text{expected_text} but got {notification.text}"
             except TimeoutException:
                 print('Unexpected error occurred!')
 
@@ -148,11 +135,11 @@ class UserPage(BaseSelenium):
 
     def delete_user_btn(self):
         """deleting user"""
-        self.select_user_delete_btn = BaseSelenium.locator_finder_by_id(self, self.select_user_delete_btn)
-        self.select_user_delete_btn.click()
+        select_user_delete_btn_sitem = self.locator_finder_by_id(self.select_user_delete_btn)
+        select_user_delete_btn_sitem.click()
 
     def confirm_delete_btn(self):
         """confirming delete user"""
-        self.select_confirm_delete_btn = BaseSelenium.locator_finder_by_id(self, self.select_confirm_delete_btn)
-        self.select_confirm_delete_btn.click()
+        select_confirm_delete_btn_sitem = self.locator_finder_by_id(self.select_confirm_delete_btn)
+        select_confirm_delete_btn_sitem.click()
         time.sleep(3)
