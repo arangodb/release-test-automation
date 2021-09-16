8fe2e2320104c38bbd3e85797b3fc0d6790a5223
Wilfried Goesgens <willi@arangodb.com>
2021-09-16 14:18:36 +0200

    nointeractive

--- jenkins/test_minio_s3.sh
+++ jenkins/test_minio_s3.sh
@@ -13,4 +13,4 @@ docker run -d \
 ulimit -n 65535
 # ./release_tester/acquire_packages.py --new-version 3.7.15-nightly --source nightlypublic --zip  --enterprise --package-dir package_cache
 
-./release_tester/test.py --new-version 3.7.15-nightly --enterprise --zip --verbose --package-dir $(pwd)/package_cache/ --test-data-dir "$(pwd)/data_dir" --starter-mode CL 
+./release_tester/test.py --new-version 3.7.15-nightly --enterprise --zip --verbose --package-dir $(pwd)/package_cache/ --test-data-dir "$(pwd)/data_dir" --starter-mode CL --no-interactive
