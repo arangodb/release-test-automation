37b15673903804557f975f3edf0f9e5efc4294c1
Vadim Kondratyev <vadim@arangodb.com>
2021-09-29 17:32:55 +0200

    Fix typo

--- jenkins/build_and_push_containers.sh
+++ jenkins/build_and_push_containers.sh
@@ -2,12 +2,12 @@
 
 DOCKER_DEB_TAG=arangodb/release-test-automation-deb-$(cat VERSION.json):$(cat containers/this_version.txt)
 docker build containers/docker_deb -t $DOCKER_DEB_TAG || exit
-docker push -t $DOCKER_DEB_TAG || exit
+docker push $DOCKER_DEB_TAG || exit
 
 DOCKER_RPM_TAG=arangodb/release-test-automation-rpm-$(cat VERSION.json):$(cat containers/this_version.txt)
 docker build containers/docker_rpm -t $DOCKER_RPM_TAG || exit
-docker push -t $DOCKER_RPM_TAG || exit
+docker push $DOCKER_RPM_TAG || exit
 
 DOCKER_TAR_TAG=arangodb/release-test-automation-tar-$(cat VERSION.json):$(cat containers/this_version.txt)
 docker build containers/docker_tar -t $DOCKER_TAR_TAG || exit
-docker push -t $DOCKER_TAR_TAG || exit
+docker push $DOCKER_TAR_TAG || exit
