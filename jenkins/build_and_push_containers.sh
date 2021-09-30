d13d8f7c2a75f9d3ddd652d742de3c7bcd45d125
Wilfried Goesgens <willi@arangodb.com>
2021-09-30 15:12:26 +0200

    fix naming

--- jenkins/build_and_push_containers.sh
+++ jenkins/build_and_push_containers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DOCKER_DEB_TAG=arangodb/release-test-automation-deb-$(cat VERSION.json):$(cat containers/this_version.txt)
+DOCKER_DEB_TAG=arangodb/release-test-automation-deb:$(cat containers/this_version.txt)
 docker build containers/docker_deb -t $DOCKER_DEB_TAG || exit
 docker push $DOCKER_DEB_TAG || exit
 
-DOCKER_RPM_TAG=arangodb/release-test-automation-rpm-$(cat VERSION.json):$(cat containers/this_version.txt)
+DOCKER_RPM_TAG=arangodb/release-test-automation-rpm:$(cat containers/this_version.txt)
 docker build containers/docker_rpm -t $DOCKER_RPM_TAG || exit
 docker push $DOCKER_RPM_TAG || exit
 
-DOCKER_TAR_TAG=arangodb/release-test-automation-tar-$(cat VERSION.json):$(cat containers/this_version.txt)
+DOCKER_TAR_TAG=arangodb/release-test-automation-tar:$(cat containers/this_version.txt)
 docker build containers/docker_tar -t $DOCKER_TAR_TAG || exit
 docker push $DOCKER_TAR_TAG || exit
