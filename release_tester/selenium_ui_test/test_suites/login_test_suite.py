1d06e4ad1cfc51e01ee21763cbaaf49c9f17cba0
Wilfried Goesgens <willi@arangodb.com>
2021-10-26 10:12:48 +0200

    login/out properly

--- release_tester/selenium_ui_test/test_suites/login_test_suite.py
+++ release_tester/selenium_ui_test/test_suites/login_test_suite.py
@@ -9,6 +9,8 @@ class LogInTestSuite(BaseTestSuite):
         """testing login page"""
         print("Starting ", self.webdriver.title, "\n")
         login_page = LoginPage(self.webdriver)
-        login_page.login_webif("root", self.root_passvoid)
-        collections_page = CollectionPage(self.webdriver)
         collections_page.log_out()
+
+        login_page.login_webif("root", self.root_passvoid)
+        login_page.log_out()
+        login_page.login_webif("root", self.root_passvoid)
