99cd7fd8f4f55daf4c8d22aa49f6cff2b08fcc3d
Wilfried Goesgens <willi@arangodb.com>
2021-11-04 15:52:06 +0100

    print loading of config yml.

--- release_tester/arangodb/installers/base.py
+++ release_tester/arangodb/installers/base.py
@@ -320,8 +320,10 @@ class InstallerBase(ABC):
         """deserialize the config from disk"""
         verbose = self.cfg.verbose
         with open(self.calc_config_file_name()) as fileh:
+            print("loading " + str(self.calc_config_file_name()))
             self.cfg = yaml.load(fileh, Loader=yaml.Loader)
         self.cfg.semver = semver.VersionInfo.parse(self.cfg.version)
+
         self.instance = ArangodInstance(
             "single",
             self.cfg.port,
