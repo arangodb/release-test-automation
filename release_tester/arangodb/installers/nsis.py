fbc5870ffc568377d596eaac7f71750e40e9806b
Wilfried Goesgens <willi@arangodb.com>
2022-02-07 13:31:10 +0100

    pdb upload for nsis as well

--- release_tester/arangodb/installers/nsis.py
+++ release_tester/arangodb/installers/nsis.py
@@ -91,7 +91,7 @@ class InstallerW(InstallerBase):
         #     architecture,
         # )
         self.client_package = None  # FIXME: Enable client package tests for NSIS when issue QA-182 is fixed
-        self.debug_package = None  # TODO
+        self.debug_package =  "ArangoDB3{ep}-{ver}.pdb.{ext}".format(**self.desc)
 
     @step
     def upgrade_server_package(self, old_installer):
