8122a1bc3ad5774d770b79bfbcef6d33da97bd0d
Wilfried Goesgens <willi@arangodb.com>
2022-03-02 07:56:42 +0100

    make it the path

--- release_tester/arangodb/installers/nsis.py
+++ release_tester/arangodb/installers/nsis.py
@@ -104,7 +104,7 @@ class InstallerW(InstallerBase):
 
     @step
     def upgrade_server_package(self, old_installer):
-        if not self.server_package.exists():
+        if not (self.cfg.package_dir / self.server_package).exists():
             raise Exception("Package not found: " + str(self.server_package))
         self._verify_signature(self.server_package)
         self.backup_dirs_number_before_upgrade = self.count_backup_dirs()
@@ -155,7 +155,7 @@ class InstallerW(InstallerBase):
 
     @step
     def install_server_package_impl(self):
-        if not self.server_package.exists():
+        if not (self.cfg.package_dir / self.server_package).exists():
             raise Exception("Package not found: " + str(self.server_package))
         self._verify_signature(self.server_package)
         cmd = [
@@ -204,7 +204,7 @@ class InstallerW(InstallerBase):
     @step
     def install_client_package_impl(self):
         """Install client package"""
-        if not self.server_package.exists():
+        if not (self.cfg.package_dir / self.server_package).exists():
             raise Exception("Package not found: " + str(self.client_package))
         self._verify_signature(self.client_package)
         cmd = [
