7e498d55758c54e1ed0c9186d9df5e9f60a7c92d
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2022-05-18 17:58:31 +0200

    Force cleanup for databasepage.py

--- release_tester/selenium_ui_test/test_suites/database_test_suite.py
+++ release_tester/selenium_ui_test/test_suites/database_test_suite.py
@@ -4,6 +4,7 @@ from selenium_ui_test.pages.database_page import DatabasePage
 from selenium_ui_test.pages.user_page import UserPage
 from selenium_ui_test.test_suites.base_selenium_test_suite import BaseSeleniumTestSuite
 from selenium_ui_test.test_suites.base_test_suite import testcase
+import traceback
 
 
 class DatabaseTestSuite(BaseSeleniumTestSuite):
@@ -16,36 +17,51 @@ class DatabaseTestSuite(BaseSeleniumTestSuite):
         # login.login('root', '')
 
         user = UserPage(self.webdriver, self.cfg)
-        assert user.current_user() == "ROOT", "current user is root?"
-        assert user.current_database() == "_SYSTEM", "current database is _system?"
-        user.user_tab()
-        user.add_new_user("tester")
-        user.add_new_user("tester01")
-
         database = DatabasePage(self.webdriver, self.cfg)
-        database.create_new_db("Sharded", 0, self.is_cluster)  # 0 = sharded DB
-        database.create_new_db("OneShard", 1, self.is_cluster)  # 1 = one shard DB
-
-        database.test_database_expected_error(self.is_cluster)  # testing expected error condition for database creation
-
-        print("Checking sorting databases to ascending and descending \n")
-        database.sorting_db()
-
-        print("Checking search database functionality \n")
-        database.searching_db("Sharded")
-        database.searching_db("OneShard")
-
-        database.deleting_database("Sharded")
-        database.deleting_database("OneShard")
-
-        # need to add delete created user here
-        user.user_tab()
-        database.deleting_user("tester")
-        database.deleting_user("tester01")
-
-        # login.logout_button()
+        self.exception = False
+        self.error = None
         assert user.current_user() == "ROOT", "current user is root?"
         assert user.current_database() == "_SYSTEM", "current database is _system?"
-        del user
-        del database
-        print("---------DataBase Page Test Completed--------- \n")
+
+        try:
+            user.user_tab()
+            user.add_new_user("tester")
+            user.add_new_user("tester01")
+
+            
+            database.create_new_db("Sharded", 0, self.is_cluster)  # 0 = sharded DB
+            database.create_new_db("OneShard", 1, self.is_cluster)  # 1 = one shard DB
+
+            database.test_database_expected_error(self.is_cluster)  # testing expected error condition for database creation
+
+            print("Checking sorting databases to ascending and descending \n")
+            database.sorting_db()
+
+            print("Checking search database functionality \n")
+            database.searching_db("Sharded")
+            database.searching_db("OneShard")
+
+            # login.logout_button()
+            assert user.current_user() == "ROOT", "current user is root?"
+            assert user.current_database() == "_SYSTEM", "current database is _system?"
+
+        except BaseException:
+            print('x' * 45, "\nINFO: Error Occurred! Force Deletion Started\n", 'x' * 45)
+            self.exception = True  # mark the exception as true
+            self.error = traceback.format_exc()
+
+        finally:
+            print("Database deletion started.")
+            database.deleting_database('Sharded')
+            database.deleting_database('OneShard')
+            # need to delete created user
+            user.user_tab()
+            database.deleting_user('tester')
+            database.deleting_user('tester01')
+
+            print("Database deletion completed.")
+            del user
+            del database
+            print("---------DataBase Page Test Completed--------- \n")
+            if self.exception:
+                raise Exception(self.error)
