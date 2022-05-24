a79b178bd4fe8aab9b7c03374981d60da2ffc1e4
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2022-05-24 10:41:46 +0200

    fixed analyzer name during deletions

--- test_data/makedata_suites/600_analyzers.js
+++ test_data/makedata_suites/600_analyzers.js
@@ -9,48 +9,48 @@
           return semver.gte(currentVersionSemver, "3.9.0") && semver.gte(oldVersionSemver, "3.9.0");
         },
 
-    makeData: function (options, isCluster, isEnterprise, dbCount, loopCount) {
-      // All items created must contain dbCount and loopCount
-      print(`making data ${dbCount} ${loopCount}`);
-      progress('create n-gram analyzer');
-      let analyzer1 = `ngram_${loopCount}`;
-      let ngram = createSafe(analyzer1,
-                             analyzer => {
-                               return a.save("trigram", "ngram", {min: 3, max: 3, preserveOriginal: false, streamType: "utf8"}, ["frequency", "norm", "position"]);
-                             }, analyzer => {
-                               return a.analyzer('trigram');
-                             }
-                            );
-    },
+        makeData: function (options, isCluster, isEnterprise, dbCount, loopCount) {
+        // All items created must contain dbCount and loopCount
+        print(`making data ${dbCount} ${loopCount}`);
+        progress('create n-gram analyzer');
+        let analyzer1 = `ngram_${loopCount}`;
+        let ngram = createSafe(analyzer1,
+                                analyzer => {
+                                return a.save("trigram", "ngram", {min: 3, max: 3, preserveOriginal: false, streamType: "utf8"}, ["frequency", "norm", "position"]);
+                                }, analyzer => {
+                                return a.analyzer('trigram');
+                                }
+                                );
+        },
 
-    checkData: function (options, isCluster, isEnterprise, dbCount, loopCount, readOnly) {
-      print(`checking data ${dbCount} ${loopCount}`);
-      // Check analyzer:  analyzers.analyzer("trigram").properties();
-      let analyzer1 = `ngram_${loopCount}`;
-      print(`Listing all analyzers in current database`)
-      a.toArray();
+        checkData: function (options, isCluster, isEnterprise, dbCount, loopCount, readOnly) {
+        print(`checking data ${dbCount} ${loopCount}`);
+        // Check analyzer:  analyzers.analyzer("trigram").properties();
+        let analyzer1 = `ngram_${loopCount}`;
+        print(`Listing all analyzers in current database`)
+        a.toArray();
 
-      print(`Checking number of analyzer is correct`)
-      if (a.toArray().length !== 14) {
-        throw new Error("Analyzer");
-      }
-      progress();
-      
-      print(`Create and use a trigram Analyzer with preserveOriginal disabled:`)
-      db._query(`RETURN TOKENS("foobar", "trigram")`).toArray();
-      
-      print(`Checking trigram analyzer properties.`)
-      a.analyzer("_system::trigram").properties();
-    },
+        print(`Checking number of analyzer is correct`)
+        if (a.toArray().length !== 14) {
+            throw new Error("Analyzer");
+        }
+        progress();
+        
+        print(`Create and use a trigram Analyzer with preserveOriginal disabled:`)
+        db._query(`RETURN TOKENS("foobar", "_system::trigram")`).toArray();
+        
+        print(`Checking trigram analyzer properties.`)
+        a.analyzer("_system::trigram").properties();
+        },
 
-    clearData: function (options, isCluster, isEnterprise, dbCount, loopCount, readOnly) {
-      print(`checking data ${dbCount} ${loopCount}`);
-      try {
-        a.remove("trigram");
-      } catch (e) {
-        print(e);
-      }
-      progress();
-    }
-  };
+        clearData: function (options, isCluster, isEnterprise, dbCount, loopCount, readOnly) {
+        print(`checking data ${dbCount} ${loopCount}`);
+        try {
+            a.remove("_system::trigram");
+        } catch (e) {
+            print(e);
+        }
+        progress();
+        }
+    };
 }());
