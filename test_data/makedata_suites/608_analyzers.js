438ca989fe7ce050e2c44b2c2877c12206820de1
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2022-05-31 11:09:34 +0200

    adding analyzer makedata/checkdata for 3.8.x version

--- test_data/makedata_suites/608_analyzers.js
+++ test_data/makedata_suites/608_analyzers.js
@@ -6,19 +6,19 @@
     isSupported: function (currentVersion, oldVersion, options, enterprise, cluster) {
       let currentVersionSemver = semver.parse(semver.coerce(currentVersion));
       let oldVersionSemver = semver.parse(semver.coerce(oldVersion));
-      return semver.gte(currentVersionSemver, "3.9.0") && semver.gte(oldVersionSemver, "3.9.0");
+      return semver.gt(currentVersionSemver, "3.8.0") && semver.lt(oldVersionSemver, "3.8.100");
     },
 
     makeDataDB: function (options, isCluster, isEnterprise, database, dbCount) {
       // All items created must contain dbCount
       print(`making per database data ${dbCount}`);
-      progress('create n-gram analyzer');
-      let analyzerName = `trigram_${dbCount}`
+      progress('create pipeline analyzer');
+      let analyzerName = `pipeline_${dbCount}`
       let trigram = createSafe(analyzerName,
         analyzer => {
-          return a.save(`${analyzerName}`, "ngram", { min: 3, max: 3, preserveOriginal: false, streamType: "utf8" }, ["frequency", "norm", "position"]);
+          return a.save(`${analyzerName}`, "pipeline", { pipeline: [{ type: "norm", properties: { locale: "en.utf-8", case: "upper" } },{ type: "ngram", properties: { min: 2, max: 2, preserveOriginal: false, streamType: "utf8" } }] }, ["frequency", "norm", "position"]);
         }, analyzer => {
-          return a.analyzer(`trigram_0`);
+          return a.analyzer(`pipeline_0`);
         });
     },
     checkDataDB: function (options, isCluster, isEnterprise, dbCount, readOnly) {
@@ -26,19 +26,19 @@
       // Check analyzer:  analyzers.analyzer("trigram").properties();
       print(`Listing all analyzers in current database`)
       a.toArray();
-      // print(`Checking number of analyzer is correct`)
+      print(`Checking number of analyzer is correct`)
       if (a.toArray().length !== 14) {
         throw new Error("Analyzer not created!");
       }
       progress();
 
-      if (a.analyzer("trigram_0") == null) {
+      if (a.analyzer("pipeline_0") == null) {
         throw new Error("Analyzer not found!");
       }
       // print(`Create and use a trigram Analyzer with preserveOriginal disabled:`)
-      db._query(`RETURN TOKENS("foobar", "trigram_0")`).toArray();
+      db._query(`RETURN TOKENS("Quick brown foX", "pipeline_0")`).toArray();
       // print(`Checking trigram analyzer properties.`)
-      a.analyzer(`trigram_0`).properties();
+      a.analyzer(`pipeline_0`).properties();
     },
     clearDataDB: function (options, isCluster, isEnterprise, dbCount, database) {
       print(`checking data ${dbCount}`);
@@ -46,8 +46,8 @@
         const array = a.toArray();
         for (let i = 0; i < array.length; i++) {
           const name = array[i];
-          if (name == `trigram_0`) {
-            a.remove(`trigram_0`);
+          if (name == `pipeline_0`) {
+            a.remove(`pipeline_0`);
           }
         }
       } catch (e) {
