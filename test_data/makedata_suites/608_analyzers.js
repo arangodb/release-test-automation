60d116ae405666d62ec7077ac6fd69d793475bfb
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2022-06-08 11:29:18 +0200

    JSlinting and adding additional check after analyzer deletion for 3.8v
    adding additional check after deleting the created analyzer during maka data process.

--- test_data/makedata_suites/608_analyzers.js
+++ test_data/makedata_suites/608_analyzers.js
@@ -1,9 +1,10 @@
 /* global print */
+/*jslint maxlen: 130 */
 
 (function () {
   const a = require("@arangodb/analyzers");
   return {
-    isSupported: function (currentVersion, oldVersion, options, enterprise, cluster) {
+    isSupported: function (currentVersion, oldVersion, options, enterprise) {
       let currentVersionSemver = semver.parse(semver.coerce(currentVersion));
       let oldVersionSemver = semver.parse(semver.coerce(oldVersion));
       return semver.gt(currentVersionSemver, "3.8.0") && semver.lt(oldVersionSemver, "3.8.100");
@@ -12,21 +13,22 @@
     makeDataDB: function (options, isCluster, isEnterprise, database, dbCount) {
       // All items created must contain dbCount
       print(`making per database data ${dbCount}`);
-      progress('create pipeline analyzer');
-      let analyzerName = `pipeline_${dbCount}`
+      progress("create pipeline analyzer");
+      let analyzerName = `pipeline_${dbCount}`;
       let trigram = createSafe(analyzerName,
-        analyzer => {
-          return a.save(`${analyzerName}`, "pipeline", { pipeline: [{ type: "norm", properties: { locale: "en.utf-8", case: "upper" } },{ type: "ngram", properties: { min: 2, max: 2, preserveOriginal: false, streamType: "utf8" } }] }, ["frequency", "norm", "position"]);
-        }, analyzer => {
+        function () {
+          return a.save(`${analyzerName}`, "pipeline", { pipeline: [{ type: "norm", properties: { locale: "en.utf-8", case: "upper" } },{ type: "ngram", properties: {
+              min: 2, max: 2, preserveOriginal: false, streamType: "utf8" } }] }, ["frequency", "norm", "position"]);
+        }, (analyzer) => {
           return a.analyzer(`pipeline_0`);
         });
     },
     checkDataDB: function (options, isCluster, isEnterprise, dbCount, readOnly) {
       print(`checking data ${dbCount}`);
       // Check analyzer:  analyzers.analyzer("trigram").properties();
-      print(`Listing all analyzers in current database`)
+      print(`Listing all analyzers in current database`);
       a.toArray();
-      print(`Checking number of analyzer is correct`)
+      print(`Checking number of analyzer is correct`);
       if (a.toArray().length !== 14) {
         throw new Error("Analyzer not created!");
       }
@@ -45,16 +47,20 @@
       try {
         const array = a.toArray();
         for (let i = 0; i < array.length; i++) {
-          const name = array[i];
-          if (name == `pipeline_0`) {
-            a.remove(`pipeline_0`);
-          }
+            const name = array[i];
+            if (name == `pipeline_0`) {
+                a.remove(`pipeline_0`);
+            }
+        // checking created analyzer is deleted or not  
+        if (a.analyzer("pipeline_0") != null) {
+          throw new Error("pipeline_0 analyzer isn't deleted yet!");
+        }
         }
       } catch (e) {
         print(e);
       }
       progress();
-    },
+    }
   };
 
 }());
