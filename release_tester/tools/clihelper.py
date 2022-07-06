c961f4688b4412ae3fe0145edbf8058f21ff193c
Vitaly Pryakhin <vitaly@arangodb.com>
2022-07-06 18:08:14 +0300

    fix minor bug

--- release_tester/tools/clihelper.py
+++ release_tester/tools/clihelper.py
@@ -9,12 +9,8 @@ import psutil
 # pylint: disable=logging-fstring-interpolation
 def run_cmd_and_log_stdout(cmd, timeout=1200):
     """run and collect the output of cmd"""
-    if type(cmd) == list:
-        cmd_str = " ".join(cmd)
-    else:
-        cmd_str = cmd
     run = psutil.Popen(cmd, stdout=PIPE, stderr=PIPE)
-    logging.info(f"running command:\n{cmd_str}\nwith PID: {run.pid}")
+    logging.info(f"running command:\n{cmd}\nwith PID: {run.pid}")
     try:
         run.wait(timeout)
     except psutil.TimeoutExpired as exc:
