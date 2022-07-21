24abf13c1a0847dfae8281b061fa27f4e7d2ac87
Wilfried Goesgens <willi@arangodb.com>
2022-07-21 17:58:00 +0200

    prepare multiarch nightly containers

--- jenkins/build_and_push_containers.sh
+++ jenkins/build_and_push_containers.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+ARCH="-$(uname -m)"
+
+if test "${ARCH}" -eq "-x86_64"; then
+    ARCH="-amd64"
+fi
+
 DOCKER_DEB_TAG=arangodb/release-test-automation-deb:$(cat containers/this_version.txt)
-docker build containers/docker_deb -t $DOCKER_DEB_TAG || exit
+docker build containers/docker_deb -t $DOCKER_DEB_TAG${ARCH} || exit
 docker push $DOCKER_DEB_TAG || exit
 
 DOCKER_RPM_TAG=arangodb/release-test-automation-rpm:$(cat containers/this_version.txt)
-docker build containers/docker_rpm -t $DOCKER_RPM_TAG || exit
+docker build containers/docker_rpm -t $DOCKER_RPM_TAG${ARCH} || exit
 docker push $DOCKER_RPM_TAG || exit
 
 DOCKER_TAR_TAG=arangodb/release-test-automation-tar:$(cat containers/this_version.txt)
-docker build containers/docker_tar -t $DOCKER_TAR_TAG || exit
+docker build containers/docker_tar -t $DOCKER_TAR_TAG${ARCH} || exit
 docker push $DOCKER_TAR_TAG || exit
+
+
+docker manifest create $DOCKER_DEB_TAG \
+--amend $DOCKER_DEB_TAG-amd64 \
+--amend $DOCKER_DEB_TAG-armv8
+docker manifest push $DOCKER_DEB_TAG
+
+docker manifest create $DOCKER_RPM_TAG \
+--amend $DOCKER_RPM_TAG-amd64 \
+--amend $DOCKER_RPM_TAG-armv8
+docker manifest push $DOCKER_TAR_TAG
+
+docker manifest create $DOCKER_TAR_TAG \
+--amend $DOCKER_TAR_TAG-amd64 \
+--amend $DOCKER_TAR_TAG-armv8
+docker manifest push $DOCKER_TAR_TAG
