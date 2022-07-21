ecc5a42e9a1820876f6c5c4520c93e7bb36e2c05
Wilfried Goesgens <willi@arangodb.com>
2022-07-21 18:31:02 +0200

    no chrome for ARM.

--- jenkins/build_and_push_containers.sh
+++ jenkins/build_and_push_containers.sh
@@ -2,14 +2,14 @@
 
 ARCH="-$(uname -m)"
 
-if test "${ARCH}" -eq "-x86_64"; then
+if test "${ARCH}" == "-x86_64"; then
     ARCH="-amd64"
 else
     ARCH="-arm64v8"
 fi
 
 DOCKER_DEB_TAG=arangodb/release-test-automation-deb:$(cat containers/this_version.txt)
-docker build containers/docker_deb -t $DOCKER_DEB_TAG${ARCH} || exit
+docker build containers/docker_deb${ARCH} -t $DOCKER_DEB_TAG${ARCH} || exit
 docker push $DOCKER_DEB_TAG${ARCH} || exit
 
 DOCKER_RPM_TAG=arangodb/release-test-automation-rpm:$(cat containers/this_version.txt)
@@ -17,7 +17,7 @@ docker build containers/docker_rpm -t $DOCKER_RPM_TAG${ARCH} || exit
 docker push $DOCKER_RPM_TAG${ARCH} || exit
 
 DOCKER_TAR_TAG=arangodb/release-test-automation-tar:$(cat containers/this_version.txt)
-docker build containers/docker_tar -t $DOCKER_TAR_TAG${ARCH} || exit
+docker build containers/docker_tar${ARCH} -t $DOCKER_TAR_TAG${ARCH} || exit
 docker push $DOCKER_TAR_TAG${ARCH} || exit
 
 
