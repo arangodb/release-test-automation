70720442ca705b7f011192d0538ed4ea9f4a13ad
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2022-08-04 11:18:02 +0200

    make the test support enterprise only

--- test_data/makedata_suites/610_analyzers.js
+++ test_data/makedata_suites/610_analyzers.js
@@ -7,7 +7,7 @@
     isSupported: function (currentVersion, oldVersion, options, enterprise, cluster) {
       let currentVersionSemver = semver.parse(semver.coerce(currentVersion));
       let oldVersionSemver = semver.parse(semver.coerce(oldVersion));
-      return semver.gte(oldVersionSemver, "3.10.0");
+      return semver.gte(oldVersionSemver, "3.10.0") && enterprise;
     },
 
     makeDataDB: function (options, isCluster, isEnterprise, database, dbCount) {
