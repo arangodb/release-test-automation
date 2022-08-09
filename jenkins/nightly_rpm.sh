2af6b95fc1b7b6c315379f857bdf5b1328bdf1c5
Wilfried Goesgens <willi@arangodb.com>
2022-08-09 17:22:21 +0200

    fix rpm

--- jenkins/nightly_rpm.sh
+++ jenkins/nightly_rpm.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+ARCH="-$(uname -m)"
+
+if test "${ARCH}" == "-x86_64"; then
+    ARCH="-amd64"
+else
+    ARCH="-arm64v8"
+fi
+
 GIT_VERSION=$(git rev-parse --verify HEAD |sed ':a;N;$!ba;s/\n/ /g')
 if test -z "$GIT_VERSION"; then
     GIT_VERSION=$VERSION
 fi
 if test -z "$OLD_VERSION"; then
-    OLD_VERSION=3.8.0-nightly
+    OLD_VERSION=3.10.0-nightly
 fi
 if test -z "$NEW_VERSION"; then
-    NEW_VERSION=3.9.0-nightly
+    NEW_VERSION=3.11.0-nightly
 fi
 if test -z "${PACKAGE_CACHE}"; then
     PACKAGE_CACHE="$(pwd)/package_cache/"
@@ -30,9 +38,6 @@ mkdir -p "${PACKAGE_CACHE}"
 mkdir -p test_dir
 mkdir -p allure-results
 
-ssh -o StrictHostKeyChecking=no -T git@github.com
-git clone git@github.com:arangodb/release-test-automation-helpers.git
-mv $(pwd)/release-test-automation-helpers $(pwd)/release_tester/tools/external_helpers
 
 DOCKER_RPM_NAME=release-test-automation-rpm
 
@@ -49,8 +54,14 @@ DOCKER_NAMESPACE="arangodb/"
 if docker pull "${DOCKER_NAMESPACE}${DOCKER_RPM_TAG}"; then
     echo "using ready built container"
 else
-    docker build containers/docker_rpm -t "${DOCKER_RPM_TAG}" || exit
+    docker build containers/docker_rpm${ARCH} -t "${DOCKER_RPM_TAG}" || exit
     DOCKER_NAMESPACE=""
+fi
+
+ssh -o StrictHostKeyChecking=no -T git@github.com
+if test ! -d $(pwd)/release_tester/tools/external_helpers; then
+  git clone git@github.com:arangodb/release-test-automation-helpers.git
+  mv $(pwd)/release-test-automation-helpers $(pwd)/release_tester/tools/external_helpers
 fi
 
 docker run \
