dc20e5c330d2ffce04827c0e929f08a469b7e917
Wilfried Goesgens <willi@arangodb.com>
2022-09-28 11:19:37 +0200

    zip

--- release_tester/arangodb/installers/tar.py
+++ release_tester/arangodb/installers/tar.py
@@ -50,7 +50,10 @@ class InstallerTAR(InstallerArchive):
 
         semdict = dict(self.cfg.semver.to_dict())
         if semdict["prerelease"]:
-            semdict["prerelease"] = "-{prerelease}".format(**semdict)
+            if semdict["prerelease"].startswith("rc"):
+                semdict["prerelease"] = "-" + semdict["prerelease"].replace("rc", "rc.")
+            else:
+                semdict["prerelease"] = "-{prerelease}".format(**semdict)
         else:
             semdict["prerelease"] = ""
         version = "{major}.{minor}.{patch}{prerelease}".format(**semdict)
