b99469912eb593ab836bb816325d58c049c11741
Wilfried Goesgens <willi@arangodb.com>
2022-11-07 13:53:42 +0100

    convert creation to data driven method as well

--- test_data/makedata_suites/609_analyzers.js
+++ test_data/makedata_suites/609_analyzers.js
@@ -1,7 +1,19 @@
 /* global print, semver, progress, createSafe, createCollectionSafe, db */
 /*jslint maxlen: 130 */
 
-function getTestData(dbcount) {
+const analyzers = require("@arangodb/analyzers");
+function createAnalyzer(analyzerName, analyzerCreationQuery){
+  // creating analyzer
+  let text = createSafe(analyzerName,
+                        function () {
+                          return analyzerCreationQuery;
+                        }, function () {
+                          if (analyzers.analyzer(analyzerName) === null) {
+                            throw new Error(`609: ${analyzerName} analyzer creation failed!`);
+                          }
+                        });
+}
+function getTestData(dbCount) {
   return [
     {
       bindVars: {
@@ -9,7 +21,18 @@ function getTestData(dbcount) {
         '@collationView': `collationEnView_${dbCount}`,
       },
       query: 'FOR doc IN @@collationView SEARCH ANALYZER(doc.text < TOKENS("c", @analyzerName)[0], @analyzerName) RETURN doc.text',
+      analyzerProperties: [
+        "collation",
+        { locale: "en.utf-8" },
+        [
+          "frequency",
+          "norm",
+          "position"
+        ]
+      ],
       collationType: "collation",
+      collection: `collationEnCol_${dbCount}`,
+      colTestData: [{ text: "a" },{ text: "å" },{ text: "b" },{ text: "z" }],
       properties: {
         "locale" : "en"
       },
@@ -27,7 +50,18 @@ function getTestData(dbcount) {
         '@collationView': `collationSvView_${dbCount}`,
       },
       query: 'FOR doc IN @@collationView SEARCH ANALYZER(doc.text < TOKENS("c", @analyzerName)[0], @analyzerName) RETURN doc.text',
+      analyzerProperties: [
+        "collation",
+        { locale: "sv.utf-8" },
+        [
+          "frequency",
+          "norm",
+          "position"
+        ]
+      ],
       collationType: "collation",
+      collection: `collationSvCol_${dbCount}`,
+      colTestData: [{ text: "a" },{ text: "å" },{ text: "b" },{ text: "z" }],
       properties: {
         "locale" : "sv"
       },
@@ -44,6 +78,16 @@ function getTestData(dbcount) {
         analyzerName: `segmentAll_${dbCount}`
       },
       query: 'LET str = "Test\twith An_EMAIL-address+123@example.org" RETURN {"all": TOKENS(str, @analyzerName),}',
+      analyzerProperties: [
+        "segmentation",
+        {
+          'break': "all"
+        }, [
+          "frequency",
+          "norm",
+          "position"
+        ]
+      ],
       collationType: "segmentation",
       properties: {
         "case" : "lower",
@@ -72,6 +116,17 @@ function getTestData(dbcount) {
         analyzerName: `segmentAlpha_${dbCount}` 
       },
       query: "LET str = 'Test\twith An_EMAIL-address+123@example.org' RETURN {'alpha': TOKENS(str, @analyzerName),}",
+      analyzerProperties: [
+        "segmentation",
+        {
+          'break': "alpha"
+        },
+        [
+          "frequency",
+          "norm",
+          "position"
+        ]
+      ],
       collationType: "segmentation",
       properties: {
         "case" : "lower",
@@ -95,6 +150,17 @@ function getTestData(dbcount) {
         analyzerName: `segmentGraphic_${dbCount}`
       },
       query: "LET str = 'Test\twith An_EMAIL-address+123@example.org' RETURN {'alpha': TOKENS(str, @analyzerName),}",
+      analyzerProperties: [
+        "segmentation",
+        {
+          'break': "graphic"
+        },
+        [
+          "frequency",
+          "norm",
+          "position"
+        ]
+      ],
       collationType: "segmentation",
       properties: {
         "case" : "lower",
@@ -132,67 +198,29 @@ function getTestData(dbcount) {
       // documentation link: https://www.arangodb.com/docs/3.9/analyzers.html
 
       print(`609: making per database data ${dbCount}`);
-      function createAnalyzer(analyzerName, analyzerCreationQuery){
-        // creating analyzer
-        let text = createSafe(analyzerName,
-                              function () {
-                                return analyzerCreationQuery;
-                              }, function () {
-                                if (a.analyzer(analyzerName) === null) {
-                                  throw new Error(`609: ${analyzerName} analyzer creation failed!`);
-                                }
-                              });
-      }
-
-      //collationEn analyzer properties
-      //collationEn Analyzer for a phonetically similar term search
-      let collationEn = `collationEn_${dbCount}`;
-      let collationEnView = `collationEnView_${dbCount}`;
-      let collationEnCol = `collationEnCol_${dbCount}`;
-      let CollationEnQuery = a.save(`${collationEn}`, "collation", { locale: "en.utf-8" }, ["frequency", "norm", "position"]);
-      let enCol = createCollectionSafe(collationEnCol, 2, 1);
-      enCol.insert([{ text: "a" },{ text: "å" },{ text: "b" },{ text: "z" },]);
-      db._createView(`${collationEnView}`, "arangosearch",{ links: { [collationEnCol]: { analyzers: [collationEn], includeAllFields: true }}});
-
-      //collationSv_ analyzer properties
-      //collationSv_ Analyzer for a phonetically similar term search
-      let collationSv = `collationSv_${dbCount}`;
-      let collationSvView = `collationSvView_${dbCount}`;
-      let collationSvCol = `collationSvCol_${dbCount}`;
-
-      let CollationSvQuery = a.save(`${collationSv}`, "collation", { locale: "sv.utf-8" }, ["frequency", "norm", "position"]);
-      let svCol = createCollectionSafe(collationSvCol, 2, 1); 
-      svCol.insert([{ text: "a" },{ text: "å" },{ text: "b" },{ text: "z" },]);
-      db._createView(`${collationSvView}`, "arangosearch",{ links: { [collationSvCol]: { analyzers: [collationSv], includeAllFields: true }}});
-
-      //segmentAll analyzer properties
-      //segmentAll Analyzer for a phonetically similar term search
-      let segmentAll = `segmentAll_${dbCount}`;
-      var all = a.save(`${segmentAll}`, "segmentation", { break: "all" }, ["frequency", "norm", "position"]);
-      let segmentAllQuery = all;
-
-      //segmentAlpha analyzer properties
-      //segmentAlpha Analyzer for a phonetically similar term search
-      let segmentAlpha = `segmentAlpha_${dbCount}`;
-      var alpha = a.save(`${segmentAlpha}`, "segmentation", { break: "alpha" }, ["frequency", "norm", "position"]);
-      let segmentAlphaQuery = alpha;
-
-      //segmentAlpha analyzer properties
-      //segmentAlpha Analyzer for a phonetically similar term search
-      let segmentGraphic = `segmentGraphic_${dbCount}`;
-      var graphic = a.save(`${segmentGraphic}`, "segmentation", { break: "graphic" }, ["frequency", "norm", "position"]);
-      let segmentGraphicQuery = graphic;
-
-      //creating collationEn  analyzer
-      createAnalyzer(collationEn, CollationEnQuery);
-      //creating collationSv  analyzer
-      createAnalyzer(collationSv, CollationSvQuery);
-      //creating segmentAll  analyzer
-      createAnalyzer(segmentAll, segmentAllQuery);
-      //creating segmentAlpha  analyzer
-      createAnalyzer(segmentAlpha, segmentAlphaQuery);
-      //creating segmentGraphic  analyzer
-      createAnalyzer(segmentGraphic, segmentGraphicQuery);
+      getTestData(dbCount).forEach(test => {
+        let q = analyzers.save(test.bindVars.analyzerName,
+                               ...test.analyzerProperties
+                              );
+        if (test.hasOwnProperty('collection')) {
+          progress(`609: creating ${test.collection} `);
+          createCollectionSafe(test.collection, 2, 1).insert(test.colTestData);
+          progress(`609: creating ${test['@collationView']} `);
+          db._createView(test.bindVars['@collationView'],
+                         "arangosearch", {
+                           links: {
+                             [test.collection]:
+                             {
+                               analyzers: [test.bindVars.analyzerName],
+                               includeAllFields: true
+                             }
+                           }
+                         }
+                        );
+        }
+        progress(`609: creating ${test.bindVars.analyzerName}`);
+        createAnalyzer(test.bindVars.analyzerName, q);
+      });
 
       return 0;
     },
@@ -253,7 +281,7 @@ function getTestData(dbcount) {
       }
 
       getTestData(dbCount).forEach(test => {
-        checkAnalyzer(test)
+        checkAnalyzer(test);
       });
       return 0;
     },
@@ -276,50 +304,26 @@ function getTestData(dbcount) {
         } catch (e) {
           print(e);
         }
-        progress();
+        progress(`609: deleted ${analyzerName}`);
       }
-
-      // declaring all the analyzer's name
-      let collationEn = `collationEn_${dbCount}`;
-      let collationSv = `collationSv_${dbCount}`;
-      let segmentAll = `segmentAll_${dbCount}`;
-      let segmentAlpha = `segmentAlpha_${dbCount}`;
-      let segmentGraphic = `segmentGraphic_${dbCount}`;
-
-      // declaring all the views name
-      let collationEnView = `collationEnView_${dbCount}`;
-      let collationSvView = `collationSvView_${dbCount}`;
-  
-
-      // deleting created Views
-      try {
-        db._dropView(collationEnView);
-        db._dropView(collationSvView);
-      } catch (e) {
-        print(e);
-      }
-
-      //deleting created collections
-      let collationEnCol = `collationEnCol_${dbCount}`;
-      let collationSvCol = `collationSvCol_${dbCount}`;
-      try {
-        db._drop(collationEnCol);
-        db._drop(collationSvCol);
-      } catch (e) {
-        print(e);
-      }
-
-      // deleting collationEn analyzer
-      deleteAnalyzer(collationEn);
-      // deleting collationSv analyzer
-      deleteAnalyzer(collationSv);
-      // deleting segmentAll analyzer
-      deleteAnalyzer(segmentAll);
-      // deleting segmentAlpha analyzer
-      deleteAnalyzer(segmentAlpha);
-      // deleting segmentGraphic analyzer
-      deleteAnalyzer(segmentGraphic);
-
+      getTestData(dbCount).forEach(test => {
+        if (test.hasOwnProperty('collection')) {
+          progress(`609: deleting view ${test.bindVars['@collationView']} `);
+          try {
+            db._dropView(test.bindVars['@collationView']);
+          } catch (ex) {
+            print(ex);
+          }
+          progress(`609: deleting collection ${test.collection} `);
+          try {
+            db._drop(test.collection);
+          } catch (ex) {
+            print(ex);
+          }
+        }
+        progress(`609: deleting Analyzer ${test.bindVars.analyzerName}`);
+        deleteAnalyzer(test.bindVars.analyzerName);
+      });
       return 0;
     }
   };
