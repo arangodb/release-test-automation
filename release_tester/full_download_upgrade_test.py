9b7a3beb97df4c1212ecac30aec7e665b545ff67
Alexey Bakharew <alex.bakharew@gmail.com>
2022-11-24 18:49:45 +0100

    revert full_download_upgrade_test.py

--- release_tester/full_download_upgrade_test.py
+++ release_tester/full_download_upgrade_test.py
@@ -39,16 +39,26 @@ def upgrade_package_test(
     list_all_processes()
     test_dir = test_driver.base_config.test_data_dir
 
+    versions = {}
     fresh_versions = {}
 
     results = []
     new_versions = []
     old_versions = []
-    versions = []
+    old_dlstages = []
+    new_dlstages = []
 
-    for v in upgrade_matrix.split(";"):
-        print("Adding: '" + v + "'")
-        versions.append(v)
+    for version_pair in upgrade_matrix.split(";"):
+        print("Adding: '" + version_pair + "'")
+        old, new = version_pair.split(":")
+        old_versions.append(old)
+        new_versions.append(new)
+        if old == primary_version:
+            old_dlstages.append(primary_dlstage)
+            new_dlstages.append(other_source)
+        else:
+            old_dlstages.append(other_source)
+            new_dlstages.append(primary_dlstage)
 
     for default_props in EXECUTION_PLAN:
         props = copy(default_props)
@@ -80,107 +90,89 @@ def upgrade_package_test(
         results.append(test_driver.run_test("all", [dl_new.cfg.version], props))
 
     # pylint: disable=consider-using-enumerate
-    for upgrade_scenario in versions:
+    for j in range(len(new_versions)):
         for props in EXECUTION_PLAN:
             print("Cleaning up" + props.testrun_name)
             test_driver.run_cleanup(props)
-        print("Cleanup done now running upgrade: " + str(upgrade_scenario).replace(":", "->"))
+        print("Cleanup done now running upgrade: " + str(old_versions[j]) + " -> " + new_versions[j])
 
-        versions_tuple = upgrade_scenario.split(":")
-        for i in range(len(versions_tuple) - 1):
-            old = versions_tuple[i]
-            new = versions_tuple[i+1]
-            print("Upgrade: ", old + "->" + new)
-            old_versions.append(old)
-            new_versions.append(new)
-            
-            old_dlstage = ""
-            new_dlstage = ""
-            if old == primary_version:
-                old_dlstage = primary_dlstage
-                new_dlstage = other_source
-            else:
-                old_dlstage = other_source
-                new_dlstage = primary_dlstage
+        # Configure Chrome to accept self-signed SSL certs and certs signed by unknown CA.
+        # FIXME: Add custom CA to Chrome to properly validate server cert.
+        # if props.ssl:
+        #    selenium_driver_args += ("ignore-certificate-errors",)
+        these_versions = []
+        for props in EXECUTION_PLAN:
+            if props.directory_suffix not in editions:
+                print("skipping " + props.directory_suffix)
+                continue
+            # pylint: disable=unused-variable
+            dl_old = Download(
+                dl_opts,
+                test_driver.base_config.hb_cli_cfg,
+                old_versions[j],
+                props.enterprise,
+                test_driver.base_config.zip_package,
+                test_driver.base_config.src_testing,
+                old_dlstages[j],
+                versions,
+                fresh_versions,
+                git_version,
+            )
+            dl_new = Download(
+                dl_opts,
+                test_driver.base_config.hb_cli_cfg,
+                new_versions[j],
+                props.enterprise,
+                test_driver.base_config.zip_package,
+                test_driver.base_config.src_testing,
+                new_dlstages[j],
+                versions,
+                fresh_versions,
+                git_version,
+            )
+            dl_old.get_packages(dl_opts.force)
+            dl_new.get_packages(dl_opts.force)
 
+            this_test_dir = test_dir / props.directory_suffix
+            test_driver.reset_test_data_dir(this_test_dir)
 
-            # Configure Chrome to accept self-signed SSL certs and certs signed by unknown CA.
-            # FIXME: Add custom CA to Chrome to properly validate server cert.
-            # if props.ssl:
-            #    selenium_driver_args += ("ignore-certificate-errors",)
-            these_versions = []
-            for props in EXECUTION_PLAN:
-                if props.directory_suffix not in editions:
-                    print("skipping " + props.directory_suffix)
-                    continue
-                # pylint: disable=unused-variable
-                dl_old = Download(
-                    dl_opts,
-                    test_driver.base_config.hb_cli_cfg,
-                    old,
-                    props.enterprise,
-                    test_driver.base_config.zip_package,
-                    test_driver.base_config.src_testing,
-                    old_dlstage,
-                    versions,
-                    fresh_versions,
-                    git_version,
-                )
-                dl_new = Download(
-                    dl_opts,
-                    test_driver.base_config.hb_cli_cfg,
-                    new,
-                    props.enterprise,
-                    test_driver.base_config.zip_package,
-                    test_driver.base_config.src_testing,
-                    new_dlstage,
-                    versions,
-                    fresh_versions,
-                    git_version,
-                )
-                dl_old.get_packages(dl_opts.force)
-                dl_new.get_packages(dl_opts.force)
+            results.append(test_driver.run_upgrade([dl_old.cfg.version, dl_new.cfg.version], props))
+            these_versions.append([dl_new.cfg.version, dl_old.cfg.version])
 
-                this_test_dir = test_dir / props.directory_suffix
-                test_driver.reset_test_data_dir(this_test_dir)
+        enterprise_packages_are_present = "EE" in editions or "EP" in editions
+        community_packages_are_present = "C" in editions
+        [new_version, old_version] = these_versions[0]
 
-                results.append(test_driver.run_upgrade([dl_old.cfg.version, dl_new.cfg.version], props))
-                these_versions.append([dl_new.cfg.version, dl_old.cfg.version])
-
-            enterprise_packages_are_present = "EE" in editions or "EP" in editions
-            community_packages_are_present = "C" in editions
-            [new_version, old_version] = these_versions[0]
-
-            if enterprise_packages_are_present and community_packages_are_present:
-                for use_enterprise in [True, False]:
-                    results.append(
-                        test_driver.run_conflict_tests(
-                            [old_version, new_version],
-                            enterprise=use_enterprise,
-                        )
-                    )
-
-            if enterprise_packages_are_present:
+        if enterprise_packages_are_present and community_packages_are_present:
+            for use_enterprise in [True, False]:
                 results.append(
-                    test_driver.run_debugger_tests(
-                        [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
-                        run_props=RunProperties(True, False, False),
+                    test_driver.run_conflict_tests(
+                        [old_version, new_version],
+                        enterprise=use_enterprise,
                     )
                 )
 
-                results.append(
-                    test_driver.run_license_manager_tests(
-                        [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
-                    )
+        if enterprise_packages_are_present:
+            results.append(
+                test_driver.run_debugger_tests(
+                    [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
+                    run_props=RunProperties(True, False, False),
                 )
+            )
 
-            if community_packages_are_present:
-                results.append(
-                    test_driver.run_debugger_tests(
-                        [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
-                        run_props=RunProperties(False, False, False),
-                    )
+            results.append(
+                test_driver.run_license_manager_tests(
+                    [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
                 )
+            )
+
+        if community_packages_are_present:
+            results.append(
+                test_driver.run_debugger_tests(
+                    [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
+                    run_props=RunProperties(False, False, False),
+                )
+            )
 
     print("V" * 80)
     status = True
