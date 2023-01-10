a39dd75a33c9f9f867ec57e6639b78983f6fa5bd
Alexey Bakharew <alex.bakharew@gmail.com>
2023-01-10 18:16:54 +0100

    Initial commit. Check presence of packages before test launch

--- release_tester/full_download_upgrade_test.py
+++ release_tester/full_download_upgrade_test.py
@@ -20,11 +20,20 @@ from tools.killall import list_all_processes
 from arangodb.installers import EXECUTION_PLAN, HotBackupCliCfg, InstallerBaseConfig, RunProperties
 
 
+# class Version:
+#     def __init__(self, name: str, source: str) -> None:
+#         self.version_name = name
+#         self.source = source
+
+# class ChainUpdate:
+#     def __init__(self, ) -> None:
+        
+
 # pylint: disable=too-many-arguments disable=too-many-locals disable=too-many-branches, disable=too-many-statements
 def upgrade_package_test(
     dl_opts: DownloadOptions,
     primary_version: str,
-    primary_dlstage: str,
+    primary_source: str,
     upgrade_matrix: str,
     other_source,
     git_version,
@@ -47,177 +56,255 @@ def upgrade_package_test(
     old_versions = []
     old_dlstages = []
     new_dlstages = []
+          
+    upgrade_scenarios = []
+    packages = {}
 
-    for version_pair in upgrade_matrix.split(";"):
-        print("Adding: '" + version_pair + "'")
-        old, new = version_pair.split(":")
-        old_versions.append(old)
-        new_versions.append(new)
-        if old == primary_version:
-            old_dlstages.append(primary_dlstage)
-            new_dlstages.append(other_source)
-        else:
-            old_dlstages.append(other_source)
-            new_dlstages.append(primary_dlstage)
+    # STEP 1: Prepare
+    for v_sequence in upgrade_matrix.split(";"):
+        versions_list = v_sequence.split(":")
+        upgrade_scenarios.append(versions_list)
+        for version_name in versions_list:
+            print(version_name)
+            if version_name in packages:
+                continue
+            else:
+                packages[version_name] = {}
+                for default_props in EXECUTION_PLAN:
+                    props = copy(default_props)
+                    props.testrun_name = "test_" + props.testrun_name
+                    if props.directory_suffix not in editions:
+                        continue
 
+                    props.directory_suffix = props.directory_suffix + "_t"
+
+                    # Verify that all required packages are exist or can be downloaded
+                    source = primary_source if primary_version == version_name else other_source
+                    res = Download(
+                        dl_opts,
+                        test_driver.base_config.hb_cli_cfg,
+                        version_name,
+                        props.enterprise,
+                        test_driver.base_config.zip_package,
+                        test_driver.base_config.src_testing,
+                        source,
+                        versions,
+                        fresh_versions,
+                        git_version,
+                    )
+                    packages[version_name][props.directory_suffix] = res
+                    res.get_packages(dl_opts.force)
+
+
+    # STEP 2: Run test for primary version
     for default_props in EXECUTION_PLAN:
         props = copy(default_props)
+        if props.directory_suffix not in editions:
+            continue
+
         props.testrun_name = "test_" + props.testrun_name
         props.directory_suffix = props.directory_suffix + "_t"
 
         test_driver.run_cleanup(props)
         print("Cleanup done")
-        if props.directory_suffix not in editions:
-            continue
-        # pylint: disable=unused-variable
-        dl_new = Download(
-            dl_opts,
-            test_driver.base_config.hb_cli_cfg,
-            primary_version,
-            props.enterprise,
-            test_driver.base_config.zip_package,
-            test_driver.base_config.src_testing,
-            primary_dlstage,
-            versions,
-            fresh_versions,
-            git_version,
-        )
-        dl_new.get_packages(dl_opts.force)
 
         this_test_dir = test_dir / props.directory_suffix
         test_driver.reset_test_data_dir(this_test_dir)
 
-        results.append(test_driver.run_test("all", "all", [dl_new.cfg.version], props))
+        results.append(test_driver.run_test("all", "all", [packages[primary_version][props.directory_suffix].cfg.version], props))    
 
-    # pylint: disable=consider-using-enumerate
-    for j in range(len(new_versions)):
-        for props in EXECUTION_PLAN:
-            print("Cleaning up" + props.testrun_name)
-            test_driver.run_cleanup(props)
-        print("Cleanup done now running upgrade: " + str(old_versions[j]) + " -> " + new_versions[j])
 
-        # Configure Chrome to accept self-signed SSL certs and certs signed by unknown CA.
-        # FIXME: Add custom CA to Chrome to properly validate server cert.
-        # if props.ssl:
-        #    selenium_driver_args += ("ignore-certificate-errors",)
-        these_versions = []
-        for props in EXECUTION_PLAN:
-            if props.directory_suffix not in editions:
-                print("skipping " + props.directory_suffix)
-                continue
-            # pylint: disable=unused-variable
-            dl_old = Download(
-                dl_opts,
-                test_driver.base_config.hb_cli_cfg,
-                old_versions[j],
-                props.enterprise,
-                test_driver.base_config.zip_package,
-                test_driver.base_config.src_testing,
-                old_dlstages[j],
-                versions,
-                fresh_versions,
-                git_version,
-            )
-            dl_new = Download(
-                dl_opts,
-                test_driver.base_config.hb_cli_cfg,
-                new_versions[j],
-                props.enterprise,
-                test_driver.base_config.zip_package,
-                test_driver.base_config.src_testing,
-                new_dlstages[j],
-                versions,
-                fresh_versions,
-                git_version,
-            )
-            dl_old.get_packages(dl_opts.force)
-            dl_new.get_packages(dl_opts.force)
 
-            this_test_dir = test_dir / props.directory_suffix
-            test_driver.reset_test_data_dir(this_test_dir)
 
-            results.append(test_driver.run_upgrade([dl_old.cfg.version, dl_new.cfg.version], props))
-            these_versions.append([dl_new.cfg.version, dl_old.cfg.version])
 
-        enterprise_packages_are_present = "EE" in editions or "EP" in editions
-        community_packages_are_present = "C" in editions
-        [new_version, old_version] = these_versions[0]
+    # STEP 3: Run upgrade tests
+    # for scenario in upgrade_scenarios:
+    #     for i in range(len(scenario)):
+    #         version_name = scenario[i]
+    #         for props in EXECUTION_PLAN:
+    #             this_test_dir = test_dir / props.directory_suffix
+    #             if i == 0:
+    #                 print("Cleaning up" + props.testrun_name)
+    #                 test_driver.run_cleanup(props)
+    #                 test_driver.reset_test_data_dir(this_test_dir)
+    #                 print("Cleanup done")
+            
+    #             results.append(test_driver.run_test("all", "all", [dl_new.cfg.version], props))    
+        
 
-        if enterprise_packages_are_present and community_packages_are_present:
-            for use_enterprise in [True, False]:
-                results.append(
-                    test_driver.run_conflict_tests(
-                        [old_version, new_version],
-                        enterprise=use_enterprise,
-                    )
-                )
+                    
 
-        if enterprise_packages_are_present:
-            results.append(
-                test_driver.run_debugger_tests(
-                    [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
-                    run_props=RunProperties(True, False, False),
-                )
-            )
 
-            results.append(
-                test_driver.run_license_manager_tests(
-                    [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
-                )
-            )
 
-        if community_packages_are_present:
-            results.append(
-                test_driver.run_debugger_tests(
-                    [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
-                    run_props=RunProperties(False, False, False),
-                )
-            )
 
-    print("V" * 80)
-    status = True
-    table = BeautifulTable(maxwidth=140)
-    for one_suite_result in results:
-        if len(one_suite_result) > 0:
-            for one_result in one_suite_result:
-                if one_result["success"]:
-                    table.rows.append(
-                        [
-                            one_result["testrun name"],
-                            one_result["testscenario"],
-                            # one_result['success'],
-                            "\n".join(one_result["messages"]),
-                        ]
-                    )
-                else:
-                    # pylint: disable=broad-except disable=bare-except
-                    try:
-                        table.rows.append(
-                            [
-                                one_result["testrun name"],
-                                one_result["testscenario"],
-                                # one_result['success'],
-                                "\n".join(one_result["messages"]) + "\n" + "H" * 40 + "\n" + one_result["progress"],
-                            ]
-                        )
-                    except Exception as ex:
-                        print("result error while syntesizing " + str(one_result))
-                        print(ex)
-                status = status and one_result["success"]
-    table.columns.header = [
-        "Testrun",
-        "Test Scenario",
-        # 'success', we also have this in message.
-        "Message + Progress",
-    ]
-    table.columns.alignment["Message + Progress"] = ALIGN_LEFT
 
-    tablestr = str(table)
-    Path("testfailures.txt").write_text(tablestr, encoding="utf8")
-    if not status:
-        print("exiting with failure")
-        sys.exit(1)
-    print(tablestr)
+
+    #=====================================================================================================
+    # 
+    # 
+    # 
+    #                 
+                    # pylint: disable=unused-variable
+                    # packages[version_name][props.directory_suffix] = Download(
+
+    #     dl_new.get_packages(dl_opts.force)
+
+    #     this_test_dir = test_dir / props.directory_suffix
+
+
+    # for default_props in EXECUTION_PLAN:
+    #     props = copy(default_props)
+    #     props.testrun_name = "test_" + props.testrun_name
+    #     props.directory_suffix = props.directory_suffix + "_t"
+
+    #     test_driver.run_cleanup(props)
+    #     print("Cleanup done")
+    #     if props.directory_suffix not in editions:
+    #         continue
+    #     # pylint: disable=unused-variable
+    #     dl_new = Download(
+    #         dl_opts,
+    #         test_driver.base_config.hb_cli_cfg,
+    #         primary_version,
+    #         props.enterprise,
+    #         test_driver.base_config.zip_package,
+    #         test_driver.base_config.src_testing,
+    #         primary_source,
+    #         versions,
+    #         fresh_versions,
+    #         git_version,
+    #     )
+
+
+    # # pylint: disable=consider-using-enumerate
+    # for j in range(len(new_versions)):
+    #     for props in EXECUTION_PLAN:
+    #         print("Cleaning up" + props.testrun_name)
+    #         test_driver.run_cleanup(props)
+    #     print("Cleanup done now running upgrade: " + str(old_versions[j]) + " -> " + new_versions[j])
+
+    #     # Configure Chrome to accept self-signed SSL certs and certs signed by unknown CA.
+    #     # FIXME: Add custom CA to Chrome to properly validate server cert.
+    #     # if props.ssl:
+    #     #    selenium_driver_args += ("ignore-certificate-errors",)
+    #     these_versions = []
+    #     for props in EXECUTION_PLAN:
+    #         if props.directory_suffix not in editions:
+    #             print("skipping " + props.directory_suffix)
+    #             continue
+    #         # pylint: disable=unused-variable
+    #         dl_old = Download(
+    #             dl_opts,
+    #             test_driver.base_config.hb_cli_cfg,
+    #             old_versions[j],
+    #             props.enterprise,
+    #             test_driver.base_config.zip_package,
+    #             test_driver.base_config.src_testing,
+    #             old_dlstages[j],
+    #             versions,
+    #             fresh_versions,
+    #             git_version,
+    #         )
+    #         dl_new = Download(
+    #             dl_opts,
+    #             test_driver.base_config.hb_cli_cfg,
+    #             new_versions[j],
+    #             props.enterprise,
+    #             test_driver.base_config.zip_package,
+    #             test_driver.base_config.src_testing,
+    #             new_dlstages[j],
+    #             versions,
+    #             fresh_versions,
+    #             git_version,
+    #         )
+    #         dl_old.get_packages(dl_opts.force)
+    #         dl_new.get_packages(dl_opts.force)
+
+    #         this_test_dir = test_dir / props.directory_suffix
+    #         test_driver.reset_test_data_dir(this_test_dir)
+
+    #         results.append(test_driver.run_upgrade([dl_old.cfg.version, dl_new.cfg.version], props))
+    #         these_versions.append([dl_new.cfg.version, dl_old.cfg.version])
+
+    #     enterprise_packages_are_present = "EE" in editions or "EP" in editions
+    #     community_packages_are_present = "C" in editions
+    #     [new_version, old_version] = these_versions[0]
+
+    #     if enterprise_packages_are_present and community_packages_are_present:
+    #         for use_enterprise in [True, False]:
+    #             results.append(
+    #                 test_driver.run_conflict_tests(
+    #                     [old_version, new_version],
+    #                     enterprise=use_enterprise,
+    #                 )
+    #             )
+
+    #     if enterprise_packages_are_present:
+    #         results.append(
+    #             test_driver.run_debugger_tests(
+    #                 [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
+    #                 run_props=RunProperties(True, False, False),
+    #             )
+    #         )
+
+    #         results.append(
+    #             test_driver.run_license_manager_tests(
+    #                 [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
+    #             )
+    #         )
+
+    #     if community_packages_are_present:
+    #         results.append(
+    #             test_driver.run_debugger_tests(
+    #                 [semver.VersionInfo.parse(old_version), semver.VersionInfo.parse(new_version)],
+    #                 run_props=RunProperties(False, False, False),
+    #             )
+    #         )
+
+    # print("version_name" * 80)
+    # status = True
+    # table = BeautifulTable(maxwidth=140)
+    # for one_suite_result in results:
+    #     if len(one_suite_result) > 0:
+    #         for one_result in one_suite_result:
+    #             if one_result["success"]:
+    #                 table.rows.append(
+    #                     [
+    #                         one_result["testrun name"],
+    #                         one_result["testscenario"],
+    #                         # one_result['success'],
+    #                         "\n".join(one_result["messages"]),
+    #                     ]
+    #                 )
+    #             else:
+    #                 # pylint: disable=broad-except disable=bare-except
+    #                 try:
+    #                     table.rows.append(
+    #                         [
+    #                             one_result["testrun name"],
+    #                             one_result["testscenario"],
+    #                             # one_result['success'],
+    #                             "\n".join(one_result["messages"]) + "\n" + "H" * 40 + "\n" + one_result["progress"],
+    #                         ]
+    #                     )
+    #                 except Exception as ex:
+    #                     print("result error while syntesizing " + str(one_result))
+    #                     print(ex)
+    #             status = status and one_result["success"]
+    # table.columns.header = [
+    #     "Testrun",
+    #     "Test Scenario",
+    #     # 'success', we also have this in message.
+    #     "Message + Progress",
+    # ]
+    # table.columns.alignment["Message + Progress"] = ALIGN_LEFT
+
+    # tablestr = str(table)
+    # Path("testfailures.txt").write_text(tablestr, encoding="utf8")
+    # if not status:
+    #     print("exiting with failure")
+    #     sys.exit(1)
+    # print(tablestr)
 
     return 0
 
