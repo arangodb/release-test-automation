e98ff05ea72cf5abdaaa9d664dfdd84418efdee7
Wilfried Goesgens <willi@arangodb.com>
2023-01-23 10:34:39 +0100

    disable logging properly

--- test_data/makedata_suites/950_read_from_follower.js
+++ test_data/makedata_suites/950_read_from_follower.js
@@ -587,6 +587,7 @@ let restoreServerLoggingSettings = function (topic) {
         }
       }
 
+      restoreServerLoggingSettings("requests");
       return 0;
       databaseName = `${baseName}_${dbCount}_entGraph`;
       db._useDatabase(databaseName);
