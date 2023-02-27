86a068cff8ea9cd60cb71dc709b3065248cc8d9e
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2023-02-27 13:23:43 +0100

    create all the CV using createCollectionSafe()

--- test_data/makedata_suites/060_computed_values.js
+++ test_data/makedata_suites/060_computed_values.js
@@ -23,41 +23,31 @@
       let a3 = createCollectionSafe(c3_insert, 3, 3, { computedValues: [{ "name": "default_insert", "expression": "RETURN SOUNDEX('frog')", computeOn: ["insert"], overwrite: true }] });
 
       let c4_update = `c4_update_060_${dbCount}`;
-      let a4 = createCollectionSafe(c4_update, 3, 3);
-      a4.properties({ computedValues: [{ "name": "default_update", "expression": "RETURN SOUNDEX('beer')", computeOn: ["update"], overwrite: true }] });
+      let a4 = createCollectionSafe(c4_update, 3, 3, { computedValues: [{ "name": "default_update", "expression": "RETURN SOUNDEX('beer')", computeOn: ["update"], overwrite: true }] });
 
       let c5_replace = `c5_replace_060_${dbCount}`;
-      let a5 = createCollectionSafe(c5_replace, 3, 3);
-      a5.properties({ computedValues: [{ "name": "default_replace", "expression": "RETURN SOUNDEX('water')", computeOn: ["replace"], overwrite: true }] });
+      let a5 = createCollectionSafe(c5_replace, 3, 3, { computedValues: [{ "name": "default_replace", "expression": "RETURN SOUNDEX('water')", computeOn: ["replace"], overwrite: true }] });
 
       let c6_not_null = `c6_not_null_060_${dbCount}`;
-      let a6 = createCollectionSafe(c6_not_null, 3, 3);
-      a6.properties({ computedValues: [{ "name": "default", "expression": "RETURN null", overwrite: true, keepNull: false }] });
+      let a6 = createCollectionSafe(c6_not_null, 3, 3, { computedValues: [{ "name": "default", "expression": "RETURN null", overwrite: true, keepNull: false }] });
 
       let c7_hex = `c7_hex_060_${dbCount}`;
-      let a7 = createCollectionSafe(c7_hex, 3, 3);
-      a7.properties({ computedValues: [{ "name": "default", "expression": "RETURN TO_HEX(@doc.name)", overwrite: true }] });
+      let a7 = createCollectionSafe(c7_hex, 3, 3, { computedValues: [{ "name": "default", "expression": "RETURN TO_HEX(@doc.name)", overwrite: true }] });
 
       let c8_overwriteFalse = `c8_overwriteFalse_060_${dbCount}`;
-      let a8 = createCollectionSafe(c8_overwriteFalse, 3, 3);
-      a8.properties({ computedValues: [{ "name": "default", "expression": "RETURN CONCAT('42_', TO_STRING(@doc.field))", overwrite: false }] });
+      let a8 = createCollectionSafe(c8_overwriteFalse, 3, 3, { computedValues: [{ "name": "default", "expression": "RETURN CONCAT('42_', TO_STRING(@doc.field))", overwrite: false }] });
 
       let c9_overwriteTrue = `c9_overwriteTrue_060_${dbCount}`;
-      let a9 = createCollectionSafe(c9_overwriteTrue, 3, 3);
-      a9.properties({ computedValues: [{ "name": "default", "expression": "RETURN CONCAT('42_', TO_STRING(@doc.field))", overwrite: true }] });
+      let a9 = createCollectionSafe(c9_overwriteTrue, 3, 3, { computedValues: [{ "name": "default", "expression": "RETURN CONCAT('42_', TO_STRING(@doc.field))", overwrite: true }] });
 
       let c10_multiple = `c10_multiple_060_${dbCount}`;
-      let a10 = createCollectionSafe(c10_multiple, 3, 3);
-      a10.properties({ computedValues: [{ "name": "default1", "expression": "RETURN 'foo'", overwrite: true }, { "name": "default2", "expression": "RETURN 'bar'", overwrite: true }, { "name": "default3", "expression": "RETURN 'baz'", overwrite: true }] });
+      let a10 = createCollectionSafe(c10_multiple, 3, 3, { computedValues: [{ "name": "default1", "expression": "RETURN 'foo'", overwrite: true }, { "name": "default2", "expression": "RETURN 'bar'", overwrite: true }, { "name": "default3", "expression": "RETURN 'baz'", overwrite: true }] });
 
       let c11 = `c11_060_${dbCount}`;
-      let a11 = createCollectionSafe(c11, 3, 3);
-      a11.properties({ computedValues: [{ "name": "default", "expression": "RETURN CONCAT(@doc._key, ' ', @doc._id, ' ', @doc._rev)", overwrite: true }] });
+      let a11 = createCollectionSafe(c11, 1, 3, { computedValues: [{ "name": "default", "expression": "RETURN CONCAT(@doc._key, ' ', @doc._id, ' ', @doc._rev)", overwrite: true }] });
 
       let c12 = `c12_060_${dbCount}`;
-      let a12 = createCollectionSafe(c12, 3, 3);
-      a12.properties({ computedValues: [{ "name": "default", "expression": "RETURN [{from_doc: CONCAT(@doc.name, ' ', @doc.field), system:{_key: @doc._key, _rev: @doc._rev, _id: @doc._id}, values: [RANGE(1, 10)]}]", overwrite: true }] });
-
+      let a12 = createCollectionSafe(c12, 3, 3, { computedValues: [{ "name": "default", "expression": "RETURN [{from_doc: CONCAT(@doc.name, ' ', @doc.field), system:{_key: @doc._key, _rev: @doc._rev, _id: @doc._id}, values: [RANGE(1, 10)]}]", overwrite: true }] });
       //-------------------------------------------------------x-------------------------------------------------------------
 
       // this function will check Computed Values properties
