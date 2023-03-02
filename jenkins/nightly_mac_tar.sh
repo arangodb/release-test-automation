b6423e3fc8c0e48edf1f369d40e050ac121373f4
Wilfried Goesgens <willi@arangodb.com>
2023-03-02 21:12:23 +0100

    fix mac invocation

--- jenkins/nightly_mac_tar.sh
+++ jenkins/nightly_mac_tar.sh
@@ -39,7 +39,7 @@ mv $(pwd)/release-test-automation-helpers $(pwd)/release_tester/tools/external_h
 
 ulimit -n 65535
 
-$(pwd)/release_tester/full_download_upgrade.py \
+python3 $(pwd)/release_tester/full_download_upgrade.py \
       --version-state-tar "${WORKSPACE}/${VERSION_TAR_NAME}" \
       --package-dir "${PACKAGE_CACHE}" \
       --old-version "${OLD_VERSION}" \
