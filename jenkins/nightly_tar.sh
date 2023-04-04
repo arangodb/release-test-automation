503e9b39d06ee4183ca0c83d4e45806a8b4a78cc
Wilfried Goesgens <willi@arangodb.com>
2023-04-04 12:05:26 +0200

    switch sequence

--- jenkins/nightly_tar.sh
+++ jenkins/nightly_tar.sh
@@ -65,13 +65,13 @@ else
 fi
 
 ssh -o StrictHostKeyChecking=no -T git@github.com
+git submodule init
+git submodule update
+
 if test ! -d $(pwd)/release_tester/tools/external_helpers; then
   git clone git@github.com:arangodb/release-test-automation-helpers.git
   mv $(pwd)/release-test-automation-helpers $(pwd)/release_tester/tools/external_helpers
 fi
-git submodule init
-git submodule update
-
 docker network create -d bridge minio-bridge
 
 
