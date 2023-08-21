c29adf1e354c9fdeb8fbc827aaef05430a3cb92f
Wilfried Goesgens <willi@arangodb.com>
2023-08-21 14:24:16 +0200

    try not to download enterprise packages twice (#389)
    * try not to download enterprise packages twice
    
    * also don't  double download here as well

--- release_tester/test.py
+++ release_tester/test.py
@@ -48,7 +48,7 @@ def main(**kwargs):
             kwargs["starter_mode"],
             [semver.VersionInfo.parse(kwargs["new_version"])],
             # pylint: disable=too-many-function-args
-            RunProperties(kwargs["enterprise"], kwargs["encryption_at_rest"], kwargs["ssl"], kwargs["replication2"]),
+            RunProperties(kwargs["enterprise"], True, kwargs["encryption_at_rest"], kwargs["ssl"], kwargs["replication2"]),
         )
         print("V" * 80)
         status = True
