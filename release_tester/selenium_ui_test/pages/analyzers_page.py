062ce9f08c9b5ca612a698eef4e6b9f7bc2aa60b
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2023-09-14 05:37:04 +0200

    fix analyzers

--- release_tester/selenium_ui_test/pages/analyzers_page.py
+++ release_tester/selenium_ui_test/pages/analyzers_page.py
@@ -709,16 +709,21 @@ class AnalyzerPage(NavigationBarPage):
             most_precise_sitem.send_keys('23')
             time.sleep(2)
         
-        print(f'Switching current view to form view for {name}\n')
-        switch_view_btn = '//*[@id="modal-content-add-analyzer"]/div[1]/div/div[2]/div/div[2]/button'
-        code_view_sitem = self.locator_finder_by_xpath(switch_view_btn)
-        code_view_sitem.click()
-        time.sleep(3)
+        #todo need to fix this one for 3.11.x
+        if self.package_version >= semver.VersionInfo.parse('3.11.0'):
+            print("skiped switching view for code view\n")
+        else:
+            print(f'Switching current view to form view for {name}\n')
+            switch_view_btn = '//*[@id="modal-content-add-analyzer"]/div[1]/div/div[2]/div/div[2]/button'
+            code_view_sitem = self.locator_finder_by_xpath(switch_view_btn)
+            code_view_sitem.click()
+            time.sleep(3)
 
-        print(f'Switching current view to code view for {name}\n')
-        form_view_sitem = self.locator_finder_by_xpath(switch_view_btn)
-        form_view_sitem.click()
-        time.sleep(3)
+            print(f'Switching current view to code view for {name}\n')
+            form_view_sitem = self.locator_finder_by_xpath(switch_view_btn)
+            form_view_sitem.click()
+            time.sleep(3)
+            
 
         print(f"Selecting the create button for the {name} \n")
         if self.package_version >= semver.VersionInfo.parse('3.11.0'):
@@ -1031,7 +1036,10 @@ class AnalyzerPage(NavigationBarPage):
                 )
 
             print(f"Closing the {name} check \n")
-            close_btn = '//*[@id="modal-content-add-analyzer"]/div[3]/button[1]'
+            if self.package_version >= semver.VersionInfo.parse('3.11.0'):
+                close_btn = '//*[@id="chakra-modal-2"]/footer/button[1]'
+            else:
+                close_btn = '//*[@id="modal-content-add-analyzer"]/div[3]/button[1]'
             close_btn_sitem = self.locator_finder_by_xpath(close_btn)
             close_btn_sitem.click()
             time.sleep(2)
@@ -1084,7 +1092,10 @@ class AnalyzerPage(NavigationBarPage):
             # force_delete.sitem = self.locator_finder_by_xpath(force_delete)
             # force_delete.sitem.click()
 
-            delete_btn = f'//*[@id="modal-content-delete-_system::{analyzer_name}"]/div[3]/button[2]'
+            if self.package_version >= semver.VersionInfo.parse('3.11.0'):
+                delete_btn = "(//button[normalize-space()='Delete'])[1]"
+            else:
+                delete_btn = f'//*[@id="modal-content-delete-_system::{analyzer_name}"]/div[3]/button[2]'
             delete_btn_sitem = self.locator_finder_by_xpath(delete_btn)
             delete_btn_sitem.click()
             time.sleep(8)
