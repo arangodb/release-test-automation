d520bf9226f4cce3fb153a37ce1b7774d11bb6e3
Vitaly Pryakhin <vitaly@arangodb.com>
2023-09-26 19:40:31 +0300

    disable minio for chain upgrade (temporarily)

--- jenkins/nightly_tar_chain.sh
+++ jenkins/nightly_tar_chain.sh
@@ -74,15 +74,15 @@ git submodule update
 docker network create -d bridge minio-bridge
 
 
-docker run -d \
-  -p 9000:9000 \
-  -p 9001:9001 \
-  --network=minio-bridge \
-  --name minio1 \
-  -v $(pwd)/test_dir/miniodata:/data \
-  -e "MINIO_ROOT_USER=minio" \
-  -e "MINIO_ROOT_PASSWORD=minio123" \
-  quay.io/minio/minio server /data --console-address ":9001"
+#docker run -d \
+#  -p 9000:9000 \
+#  -p 9001:9001 \
+#  --network=minio-bridge \
+#  --name minio1 \
+#  -v $(pwd)/test_dir/miniodata:/data \
+#  -e "MINIO_ROOT_USER=minio" \
+#  -e "MINIO_ROOT_PASSWORD=minio123" \
+#  quay.io/minio/minio server /data --console-address ":9001"
 
 
 # we need --init since our upgrade leans on zombies not happening:
@@ -98,10 +98,10 @@ docker run \
        --env="PYTHONUNBUFFERED=1" \
        --env="RTA_LOCAL_HTTPUSER=${RTA_LOCAL_HTTPUSER}" \
        --env="WORKSPACE=/home/release-test-automation/" \
-       --env="AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
-       --env="AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
-       --env="AWS_REGION=$AWS_REGION" \
-       --env="AWS_ACL=$AWS_ACL" \
+#       --env="AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+#       --env="AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+#       --env="AWS_REGION=$AWS_REGION" \
+#       --env="AWS_ACL=$AWS_ACL" \
        \
        --network=minio-bridge \
        --name="${DOCKER_TAR_NAME}" \
@@ -117,7 +117,7 @@ docker run \
           --release-tracker-username ${RELEASE_TRACKER_USERNAME} \
           --release-tracker-password ${RELEASE_TRACKER_PASSWORD} \
           --zip \
-          --hb-mode s3bucket \
+#          --hb-mode s3bucket \
           --verbose \
           --alluredir /home/allure-results \
           --git-version "$GIT_VERSION" \
