524100cf13d846de951d87001836ac42cd23d24a
Vitaly Pryakhin <vitaly@arangodb.com>
2023-09-26 13:54:30 +0300

    increase number of memory mappings in linux entrypoint scripts

--- jenkins/nightly_qa_tester.sh
+++ jenkins/nightly_qa_tester.sh
@@ -45,6 +45,11 @@ fi
 git submodule init
 git submodule update
 
+echo "Maximum number of memory mappings per process is: `cat /proc/sys/vm/max_map_count`"
+echo "Setting maximum number of memory mappings per process to: $((`nproc`*8*8000))"
+sudo sysctl -w "vm.max_map_count=$((`nproc`*8*8000))"
+echo "Maximum number of memory mappings per process is: `cat /proc/sys/vm/max_map_count`"
+
 # we need --init since our upgrade leans on zombies not happening:
 docker run \
        --ulimit core=-1 \
