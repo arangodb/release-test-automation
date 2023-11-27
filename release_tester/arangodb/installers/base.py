44a76a5797032ebbe96e8f2b166e2477971b0286
Wilfried Goesgens <willi@arangodb.com>
2023-11-27 10:05:43 +0100

    ignore proper suffix

--- release_tester/arangodb/installers/base.py
+++ release_tester/arangodb/installers/base.py
@@ -131,7 +131,7 @@ class BinaryDescription:
 
     def _friendly_name(self, string):
         """ should find friendly name in string """
-        return string == self.friendly_name
+        return re.match(string, self.friendly_name) is not None
 
     def _validate_windows_attributes(self, version, enterprise):
         """ validate the windows binary header fields """
