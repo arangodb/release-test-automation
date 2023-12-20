1510dd2d599d68dc45b8d251eaac29d20b77d338
Mohammad-Al Fattah <35941919+MDAlFattah@users.noreply.github.com>
2023-12-20 03:06:33 +0100

    Update service_page.py

--- release_tester/selenium_ui_test/pages/service_page.py
+++ release_tester/selenium_ui_test/pages/service_page.py
@@ -507,19 +507,19 @@ class ServicePage(NavigationBarPage):
                 graphql_service_sitem.click()
                 time.sleep(2)
                 break
-            except NoSuchElementException  as ex:
+            except NoSuchElementException:
                 print(f'Error occurred while selecting graphql service. Attempt {attempt} \n')
 
                 time.sleep(2)
                 print(f"Attempt {attempt}: Element not found. Retrying...")
                 if attempt == max_retries:
                     print("Maximum retries reached. Exiting.")
-                    raise  # Re-raise the exception if max retries reached
+                    raise
                 else:
                     print('Refreshing the UI \n')
                     self.webdriver.refresh()
                     time.sleep(2)
-                    continue  # Retry the loop
+                    continue
 
         print('Opening graphql interface \n')
         graphql_interface = '//*[@id="information"]/div/div[2]/div[2]/input'
