c385dd8482a96f5d85e5074fdc5cde5b52de38bc
Wilfried Goesgens <willi@arangodb.com>
2024-01-11 14:02:56 +0100

    missing quotes

--- jenkins/common/load_git_submodules.sh
+++ jenkins/common/load_git_submodules.sh
@@ -1,7 +1,7 @@
 ssh -o StrictHostKeyChecking=no -T git@github.com
-if test ! -d $(pwd)/release_tester/tools/external_helpers; then
+if test ! -d "$(pwd)/release_tester/tools/external_helpers"; then
     git clone git@github.com:arangodb/release-test-automation-helpers.git
-    mv $(pwd)/release-test-automation-helpers $(pwd)/release_tester/tools/external_helpers
+    mv "$(pwd)/release-test-automation-helpers" "$(pwd)/release_tester/tools/external_helpers"
 fi
 git submodule init
 git submodule update
