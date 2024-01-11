38a4f9adda1790124fa9032abb0ad3dbdb090578
Wilfried Goesgens <willi@arangodb.com>
2024-01-11 18:45:18 +0100

    increase timeout - we have longer browser idle times

--- jenkins/common/setup_selenium.sh
+++ jenkins/common/setup_selenium.sh
@@ -4,10 +4,13 @@ if [[ ${argv[@]} =~ "--selenium" ]]; then
                       --network="${DOCKER_NETWORK_NAME}" \
                       -v /var/run/docker.sock:/var/run/docker.sock\
                       -v "$(pwd)/selenoid_config/:/etc/selenoid/:ro" \
-                      aerokube/selenoid:latest-release -container-network "${DOCKER_NETWORK_NAME}" )
+                      aerokube/selenoid:latest-release -timeout 60m -container-network "${DOCKER_NETWORK_NAME}" )
     SELENOID_IP=$(docker inspect \
                          -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${SELENOID})
     echo "Selenoid IP: ${SELENOID_IP}"
+    if test -z "${SELENOID_IP}"; then
+        exit 1
+    fi
     docker pull selenoid/chrome
     RTA_ARGS+=( --selenium-driver-args "command_executor=http://${SELENOID_IP}:4444/wd/hub")
     DOCKER_SELENOID_CLEANUP1="docker stop ${SELENOID}"
