11467b078c390297ce7c0eb1248e92da5df944ad
Wilfried Goesgens <willi@arangodb.com>
2024-05-08 20:15:11 +0200

    why is that file empty?

--- jenkins/common/gather_coredumps.sh
+++ jenkins/common/gather_coredumps.sh
@@ -1,6 +1,9 @@
 if [ `ls -1 "$(pwd)/test_dir/core"* 2>/dev/null | wc -l ` -gt 0 ]; then
     7z a coredumps "$(pwd)/test_dir/core"*
-    printf "\nCoredumps found after testrun:\n $(ls -l "$(pwd)/test_dir/core"*)\n" >> "$(pwd)/test_dir/testfailures.txt"
+    (
+        cat "$(pwd)/test_dir/testfailures.txt";
+        printf "\nCoredumps found after testrun:\n $(ls -l "$(pwd)/test_dir/core"*)\n"
+    ) > "$(pwd)/test_dir/testfailures.txt"
     rm -f "$(pwd)/test_dir/core"*
     mv coredumps.7z "$(pwd)/test_dir/"
     echo "FAILED BY COREDUMP FOUND!"
