901fe31ec9459bb59efcdb35123da10c39d60f19
Vitaly Pryakhin <vitaly@arangodb.com>
2024-05-31 15:01:37 +0300

    allow to set upgrade matrix manually for chain upgrade

--- release_tester/run_chain_upgrade.py
+++ release_tester/run_chain_upgrade.py
@@ -9,8 +9,15 @@ from pathlib import Path
 import click
 
 from arangodb.installers import HotBackupCliCfg, InstallerBaseConfig
-from common_options import very_common_options, common_options, download_options, full_common_options, \
-    hotbackup_options, ui_test_suite_filtering_options
+from common_options import (
+    very_common_options,
+    common_options,
+    download_options,
+    full_common_options,
+    hotbackup_options,
+    ui_test_suite_filtering_options,
+    matrix_options,
+)
 from download import DownloadOptions
 from full_download_upgrade_test import upgrade_package_test
 from test_driver import TestDriver
@@ -30,6 +37,7 @@ from tools.release_tracker_client.client import ReleaseTrackerApiClient, OS, Arc
 @click.option("--release-tracker-password", default="", help="password for the release tracker API")
 @very_common_options()
 @hotbackup_options()
+@matrix_options()
 @common_options(
     support_multi_version=False,
     support_old=False,
@@ -53,45 +61,49 @@ def main(**kwargs):
     kwargs['base_config'] = InstallerBaseConfig.from_dict(**kwargs)
     dl_opts = DownloadOptions.from_dict(**kwargs)
 
-    os = None
-    if platform.win32_ver()[0] != "":
-        os = OS.WINDOWS
-    elif platform.mac_ver()[0] != "":
-        os = OS.MACOS
-    elif platform.system() in ["linux", "Linux"]:
-        os = OS.LINUX
+    upgrade_matrix = None
+    if kwargs['upgrade_matrix'] == "":
+        os = None
+        if platform.win32_ver()[0] != "":
+            os = OS.WINDOWS
+        elif platform.mac_ver()[0] != "":
+            os = OS.MACOS
+        elif platform.system() in ["linux", "Linux"]:
+            os = OS.LINUX
+        else:
+            raise Exception("Unable to detect OS.")
+        cpu_arch = None
+        if platform.uname().machine.lower() in ["x86_64", "amd64"]:
+            cpu_arch = Arch.X86
+        else:
+            cpu_arch = Arch.ARM
+        release_tracker_client = ReleaseTrackerApiClient(host=kwargs['release_tracker_hostname'],
+                                                         port=kwargs['release_tracker_port'],
+                                                         protocol=kwargs['release_tracker_protocol'],
+                                                         username=kwargs['release_tracker_username'],
+                                                         password=kwargs['release_tracker_password'])
+        nightly_branches = release_tracker_client.nightly_branches()
+        oldest_branch = nightly_branches[0]
+        devel_branch = nightly_branches[-1]
+        upgrade_list = []
+        upgrade_list.append(release_tracker_client.get_latest_release_if_any(oldest_branch, os, cpu_arch))
+        upgrade_list.append(release_tracker_client.get_latest_nightly_if_any(oldest_branch, os, cpu_arch))
+        for branch in nightly_branches[1:-1]:
+            upgrade_list.append(release_tracker_client.get_latest_release_if_any(branch, os, cpu_arch))
+            upgrade_list.append(release_tracker_client.get_latest_nightly_if_any(branch, os, cpu_arch))
+        upgrade_list.append(release_tracker_client.get_latest_release_if_any(devel_branch, os, cpu_arch))
+        upgrade_list.append(release_tracker_client.get_latest_nightly_if_any(devel_branch, os, cpu_arch))
+        while None in upgrade_list:
+            upgrade_list.remove(None)
+        upgrade_matrix = ":".join(upgrade_list)
     else:
-        raise Exception("Unable to detect OS.")
-    cpu_arch = None
-    if platform.uname().machine.lower() in ["x86_64", "amd64"]:
-        cpu_arch = Arch.X86
-    else:
-        cpu_arch = Arch.ARM
-    release_tracker_client = ReleaseTrackerApiClient(host=kwargs['release_tracker_hostname'],
-                                                     port=kwargs['release_tracker_port'],
-                                                     protocol=kwargs['release_tracker_protocol'],
-                                                     username=kwargs['release_tracker_username'],
-                                                     password=kwargs['release_tracker_password'])
-    nightly_branches = release_tracker_client.nightly_branches()
-    oldest_branch = nightly_branches[0]
-    devel_branch = nightly_branches[-1]
-    upgrade_list = []
-    upgrade_list.append(release_tracker_client.get_latest_release_if_any(oldest_branch, os, cpu_arch))
-    upgrade_list.append(release_tracker_client.get_latest_nightly_if_any(oldest_branch, os, cpu_arch))
-    for branch in nightly_branches[1:-1]:
-        upgrade_list.append(release_tracker_client.get_latest_release_if_any(branch, os, cpu_arch))
-        upgrade_list.append(release_tracker_client.get_latest_nightly_if_any(branch, os, cpu_arch))
-    upgrade_list.append(release_tracker_client.get_latest_release_if_any(devel_branch, os, cpu_arch))
-    upgrade_list.append(release_tracker_client.get_latest_nightly_if_any(devel_branch, os, cpu_arch))
-    while None in upgrade_list:
-        upgrade_list.remove(None)
-    upgrade_matrix = ":".join(upgrade_list)
+        upgrade_matrix = kwargs['upgrade_matrix']
 
     test_driver = TestDriver(**kwargs)
     try:
         results = upgrade_package_test(
             dl_opts,
-            new_version,
+            None,
             kwargs['source'],
             upgrade_matrix,
             kwargs['other_source'],
