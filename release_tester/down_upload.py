24b821f8de6e14c1ba47f2cdaa38d75d763b39cf
Wilfried Goesgens <willi@arangodb.com>
2024-06-27 11:09:19 +0200

    add multi host support

--- release_tester/down_upload.py
+++ release_tester/down_upload.py
@@ -20,7 +20,7 @@ import tools.loghelper as lh
 @click.command()
 @very_common_options()
 @download_options()
-@click.option('--push-host', 'push_host', type=click.STRING)
+@click.option('--push-hosts', 'push_hosts', type=click.STRING, multiple=True,   default=[])
 @click.option('--push-user', 'push_user', type=click.STRING, default='tester')
 @click.option('--ssh-key-file', 'ssh_key_file', type=click.STRING, default='~/.ssh/id_rsa.pub')
 @click.option('--upload-path', 'upload_path', type=click.STRING, default='~/release-test-automation/package_cache')
@@ -41,38 +41,49 @@ def main(**kwargs):
     kwargs['hb_cli_cfg'] = HotBackupCliCfg("disabled","","","","","","")
 
     kwargs['test'] = ''
-    kwargs['base_config'] = InstallerBaseConfig.from_dict(**kwargs)
+    for one_host in kwargs['push_hosts']:
+        try:
+            (
+                kwargs['push_host'],
+                kwargs['force_arch'],
+                kwargs['force_os']
+            ) = one_host.split(':')
+        except Exception as ex:
+            print(f"--push-hosts syntax 'host:arch:os' have {one_host} - {ex}")
+            sys.exit(1)
 
-    dl_opts = DownloadOptions.from_dict(**kwargs)
-    lh.configure_logging(kwargs['verbose'])
-    pw = ''
-    if 'PASSVOID' in os.environ:
-        pw=os.environ['PASSVOID']
-    client = RemoteClient(kwargs['push_host'],
-                          kwargs['push_user'],
-                          pw,
-                          kwargs['ssh_key_file'],
-                          kwargs['upload_path'])
-    client.execute_commands([
-        f'if test ! -d release-test-automation; then git clone https://github.com/arangodb/release-test-automation.git; else cd release-test-automation; git pull --all; fi',
-        f'mkdir -p {kwargs["upload_path"]}'
-    ])
+        kwargs['base_config'] = InstallerBaseConfig.from_dict(**kwargs)
 
-    for zipit in [True, False]:
-        for com_ep in [True, False]:
-            downloader = Download(
-                options=dl_opts,
-                hb_cli_cfg=kwargs['hb_cli_cfg'],
-                version=kwargs['new_version'],
-                enterprise=com_ep,
-                zip_package=zipit,
-                src_testing=kwargs['src_testing'],
-                source=kwargs['source'],
-                force_arch=kwargs['force_arch'],
-                force_os=kwargs['force_os'])
-            packages = downloader.get_packages(kwargs['force'])
-            print(packages)
-            client.bulk_upload(packages)
+        dl_opts = DownloadOptions.from_dict(**kwargs)
+        lh.configure_logging(kwargs['verbose'])
+        pw = ''
+        if 'PASSVOID' in os.environ:
+            pw=os.environ['PASSVOID']
+        client = RemoteClient(kwargs['push_host'],
+                              kwargs['push_user'],
+                              pw,
+                              kwargs['ssh_key_file'],
+                              kwargs['upload_path'])
+        client.execute_commands([
+            'if test ! -d release-test-automation; then git clone https://github.com/arangodb/release-test-automation.git; else cd release-test-automation; git pull --all; fi',
+            f'mkdir -p {kwargs["upload_path"]}'
+        ])
+
+        for zipit in [True, False]:
+            for com_ep in [True, False]:
+                downloader = Download(
+                    options=dl_opts,
+                    hb_cli_cfg=kwargs['hb_cli_cfg'],
+                    version=kwargs['new_version'],
+                    enterprise=com_ep,
+                    zip_package=zipit,
+                    src_testing=kwargs['src_testing'],
+                    source=kwargs['source'],
+                    force_arch=kwargs['force_arch'],
+                    force_os=kwargs['force_os'])
+                packages = downloader.get_packages(kwargs['force'])
+                print(packages)
+                client.bulk_upload(packages)
     # client.disconnect()
 
 
