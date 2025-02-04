fa78c1381498ee6835b18848634439041c98beea
Wilfried Goesgens <willi@arangodb.com>
2025-02-04 14:40:14 +0100

    enable vector index

--- release_tester/arangodb/starter/manager.py
+++ release_tester/arangodb/starter/manager.py
@@ -95,6 +95,10 @@ class StarterManager:
                 "--all.rclone.argument=--log-level=DEBUG",
                 "--all.rclone.argument=--log-file=@ARANGODB_SERVER_DIR@/rclone.log",
             ]
+        if self.cfg.semver > "3.12.4":
+            self.moreopts += [
+                "--all.experimental-vector-index=true",
+            ]
         print(self.moreopts)
         # directories
         self.raw_basedir = install_prefix
