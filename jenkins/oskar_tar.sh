455f2f7c3cee2678b6ebc6026ff18122adb19958
Wilfried Goesgens <willi@arangodb.com>
2025-02-18 13:15:13 +0100

    forward SAN_MODE

--- jenkins/oskar_tar.sh
+++ jenkins/oskar_tar.sh
@@ -96,7 +96,10 @@ if test -n "${COVERAGE}"; then
   DOCKER_ARGS+=(-e "COVERAGE=${COVERAGE}")
 fi
 if test -n "${SAN}"; then
-  DOCKER_ARGS+=(-e "SAN=${SAN}")
+    DOCKER_ARGS+=(
+        -e "SAN=${SAN}"
+        -e "SAN_MODE="${SAN_MODE}"
+    )
 fi
 # we need --init since our upgrade leans on zombies not happening:
 docker run \
