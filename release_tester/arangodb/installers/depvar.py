b981b4e12a8a7a8d93ee2b28cfb747d48b848bfb
Wilfried Goesgens <willi@arangodb.com>
2026-07-24 15:45:54 +0200

    cleanup - delete unsupported deployments

--- release_tester/arangodb/installers/depvar.py
+++ release_tester/arangodb/installers/depvar.py
@@ -9,39 +9,39 @@ class RunnerType(Enum):
 
     NONE = 0
     SINGLE = 1
-    LEADER_FOLLOWER = 2
-    ACTIVE_FAILOVER = 3
+#    LEADER_FOLLOWER = 2
+#    ACTIVE_FAILOVER = 3
     CLUSTER = 4
-    DC2DC = 5
-    DC2DCENDURANCE = 6
+#    DC2DC = 5
+#    DC2DCENDURANCE = 6
     TESTING = 7
 
 
 runner_strings = {
     RunnerType.NONE: "none",
     RunnerType.SINGLE: "Single Server",
-    RunnerType.LEADER_FOLLOWER: "Leader / Follower",
-    RunnerType.ACTIVE_FAILOVER: "Active Failover",
+#    RunnerType.LEADER_FOLLOWER: "Leader / Follower",
+#    RunnerType.ACTIVE_FAILOVER: "Active Failover",
     RunnerType.CLUSTER: "Cluster",
-    RunnerType.DC2DC: "DC 2 DC",
-    RunnerType.DC2DCENDURANCE: "DC 2 DC endurance",
+#    RunnerType.DC2DC: "DC 2 DC",
+#    RunnerType.DC2DCENDURANCE: "DC 2 DC endurance",
     RunnerType.TESTING: "Testing",
 }
 
 STARTER_MODES = {
     "all": [
         RunnerType.SINGLE,
-        RunnerType.LEADER_FOLLOWER,
-        RunnerType.ACTIVE_FAILOVER,
+#        RunnerType.LEADER_FOLLOWER,
+#        RunnerType.ACTIVE_FAILOVER,
         RunnerType.CLUSTER,
-        RunnerType.DC2DC,
+#        RunnerType.DC2DC,
     ],
     "SG": [RunnerType.SINGLE],
-    "LF": [RunnerType.LEADER_FOLLOWER],
-    "AFO": [RunnerType.ACTIVE_FAILOVER],
+#    "LF": [RunnerType.LEADER_FOLLOWER],
+#    "AFO": [RunnerType.ACTIVE_FAILOVER],
     "CL": [RunnerType.CLUSTER],
-    "DC": [RunnerType.DC2DC],
-    "DCendurance": [RunnerType.DC2DCENDURANCE],
+#    "DC": [RunnerType.DC2DC],
+#    "DCendurance": [RunnerType.DC2DCENDURANCE],
     "none": [RunnerType.NONE],
 }
 
