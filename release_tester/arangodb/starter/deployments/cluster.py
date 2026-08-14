6d0979a3f16344cca0b52939c7947441f9a8b2e5
Wilfried Goesgens <willi@arangodb.com>
2026-08-14 10:38:14 +0200

    fix json generation

--- release_tester/arangodb/starter/deployments/cluster.py
+++ release_tester/arangodb/starter/deployments/cluster.py
@@ -251,7 +251,7 @@ class Cluster(Runner):
                     InstanceType.COORDINATOR,
                     requests.post,
                     '/_admin/cluster/resignLeadership',
-                    { "server": uuid, "undoMoves": False })
+                    f'{{ "server": "{uuid}", "undoMoves": False }}')
                 if reply[0].status_code != 500 :
                     print(f"retrying resign leadership - {reply[0].status_code} - {reply[0].content}")
                     break
